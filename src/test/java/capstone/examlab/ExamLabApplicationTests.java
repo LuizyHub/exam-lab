56b7be2550e9b3a38125df3e83bb3502e11f8fc7
luizy <luizy991212@gmail.com>
2024-02-11 22:43:23 +0900

    build: spring initialize
    [#13]

---
+++ src/test/java/capstone/examlab/ExamLabApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.examlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

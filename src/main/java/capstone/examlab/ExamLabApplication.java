a972fdb61fc48dd6366bcbf00e917539662e0741
luizy <luizy991212@gmail.com>
2024-02-11 22:43:23 +0900

    build: spring initialize
    [#13]

---
+++ src/main/java/capstone/examlab/ExamLabApplication.java
@@ -0,0 +1,13 @@
+package capstone.examlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamLabApplication.class, args);
+	}
+
+}

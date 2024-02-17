ce8a708dc5ef7f313cc415c4aa84d7ff2d31749d
luizy <luizy991212@gmail.com>
2024-02-18 00:12:10 +0900

    feat: ExamsService 인터페이스 정의
    [#15]

---
+++ src/main/java/capstone/examlab/exams/service/ExamsService.java
@@ -0,0 +1,15 @@
+package capstone.examlab.exams.service;
+
+import capstone.examlab.exams.dto.ExamList;
+import capstone.examlab.exams.dto.ExamType;
+import capstone.examlab.exams.dto.QuestionsList;
+import capstone.examlab.exams.dto.QuestionsOption;
+
+public interface ExamsService {
+
+    public ExamType getExamType(Long id);
+
+    public ExamList getExamList();
+
+    public QuestionsList getQuestionsList(QuestionsOption questionsOption);
+}

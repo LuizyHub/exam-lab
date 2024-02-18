31ad67f44ece0944682091a6e5d8a81c3c3fd73e
luizy <luizy991212@gmail.com>
2024-02-18 22:15:44 +0900

    feature: QuestionsOption Dto Builder 패턴 적용
    - 테스트 코드 작성의 용이함을 위해 생성
    
    [#15]

--- src/main/java/capstone/examlab/exams/dto/QuestionsOption.java
+++ src/main/java/capstone/examlab/exams/dto/QuestionsOption.java
@@ -1,10 +1,12 @@
 package capstone.examlab.exams.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Builder
 public class QuestionsOption {
     private List<String> tags;
     private Integer count;

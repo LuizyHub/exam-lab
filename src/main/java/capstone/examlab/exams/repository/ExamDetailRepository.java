3de8454cc7b3725ce33788de951774c759c90370
jisu-0305 <comjisu0311@naver.com>
2024-02-22 23:01:58 +0900

    feat: add entity related to exam classification  #18

---
+++ src/main/java/capstone/examlab/exams/repository/ExamDetailRepository.java
@@ -0,0 +1,8 @@
+package capstone.examlab.exams.repository;
+
+import capstone.examlab.exams.entity.ExamDetailEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExamDetailRepository extends JpaRepository<ExamDetailEntity, Integer> {
+
+}

7f4db948b288c7de40d90e549bf7748dc89dd1e8
jisu-0305 <comjisu0311@naver.com>
2024-03-02 04:47:41 +0900

    feat: 기존 예시용 Controller, Serivce 구현 및 관련 DTO수정 #21

--- src/main/java/capstone/examlab/exams/dto/Question.java
+++ src/main/java/capstone/examlab/exams/dto/Question.java
@@ -8,15 +8,32 @@ import java.util.ArrayList;
 @Data
 @Builder
 public class Question {
-    private Long id;
+    private String id;
     private String type;
     private String question;
     private ArrayList<String> questionImageUrls;
     private ArrayList<String> questionImageDescriptions;
     private ArrayList<String> options;
     private ArrayList<Integer> answers;
-    private String explanation;
-    private ArrayList<String> explanationImageUrls;
-    private ArrayList<String> explanationImageDescriptions;
+    private String commentary;
+    private ArrayList<String> commentaryImageUrls;
+    private ArrayList<String> commentaryImageDescriptions;
     private ArrayList<String> tags;
+
+    public Question() {
+    }
+
+    public Question(String id, String type, String question, ArrayList<String> questionImageUrls, ArrayList<String> questionImageDescriptions, ArrayList<String> options, ArrayList<Integer> answers, String commentary, ArrayList<String> commentaryImageUrls, ArrayList<String> commentaryImageDescriptions, ArrayList<String> tags) {
+        this.id = id;
+        this.type = type;
+        this.question = question;
+        this.questionImageUrls = questionImageUrls;
+        this.questionImageDescriptions = questionImageDescriptions;
+        this.options = options;
+        this.answers = answers;
+        this.commentary = commentary;
+        this.commentaryImageUrls = commentaryImageUrls;
+        this.commentaryImageDescriptions = commentaryImageDescriptions;
+        this.tags = tags;
+    }
 }

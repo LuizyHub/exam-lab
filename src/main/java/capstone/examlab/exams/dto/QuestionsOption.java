7529d7d146d959cae73f9fb8351ac0023523e36f
jisu-0305 <comjisu0311@naver.com>
2024-03-01 14:49:19 +0900

    fix: QuestionOption List기본 생성자 선언 #21
    ExamsControllerTest의 오류를 통해서 해당 문제 찾은 후 해결

--- src/main/java/capstone/examlab/exams/dto/QuestionsOption.java
+++ src/main/java/capstone/examlab/exams/dto/QuestionsOption.java
@@ -3,6 +3,7 @@ package capstone.examlab.exams.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -11,4 +12,17 @@ public class QuestionsOption {
     private List<String> tags;
     private Integer count;
     private List<String> includes;
+
+    // 기본 생성자와 Builder 생성자에 ArrayList 사용
+    public QuestionsOption() {
+        this.tags = new ArrayList<>();
+        this.includes = new ArrayList<>();
+    }
+
+    @Builder
+    public QuestionsOption(List<String> tags, Integer count, List<String> includes) {
+        this.tags = tags;
+        this.count = count;
+        this.includes = includes;
+    }
 }

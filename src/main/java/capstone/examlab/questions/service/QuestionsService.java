35b71227a0ef0d092ee6a4be8feab234e6d75961
jisu-0305 <comjisu0311@naver.com>
2024-03-02 04:47:41 +0900

    feat: 기존 예시용 Controller, Serivce 구현 및 관련 DTO수정 #21

--- src/main/java/capstone/examlab/questions/service/QuestionsService.java
+++ src/main/java/capstone/examlab/questions/service/QuestionsService.java
@@ -1,10 +1,9 @@
 package capstone.examlab.questions.service;
 
-import capstone.examlab.exams.dto.ExamList;
-import capstone.examlab.questions.entity.QuestionEntity;
+import capstone.examlab.exams.dto.QuestionsList;
+import capstone.examlab.exams.dto.QuestionsOption;
 
-import java.util.List;
 
 public interface QuestionsService {
-    List<QuestionEntity> findByDriverLicenseQuestions(List<String> tags, int count, String includes);
+    QuestionsList findByDriverLicenseQuestions(QuestionsOption questionsOption);
 }

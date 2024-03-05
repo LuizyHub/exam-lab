3ade5ef636a81491018ea9bb83d30ec53680843e
luizy <luizy991212@gmail.com>
2024-03-06 02:01:22 +0900

    refactor: exams와 questions 도메인 분리

---
+++ src/main/java/capstone/examlab/questions/dto/Image.java
@@ -0,0 +1,10 @@
+package capstone.examlab.questions.dto;
+
+import lombok.Data;
+
+@Data
+public class Image {
+    private String url;
+    private String description;
+    private String attribute;
+}

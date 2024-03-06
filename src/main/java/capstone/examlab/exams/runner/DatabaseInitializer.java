74cb2c001cee642834d6d0501d91be0b0f7ffe03
luizy <luizy991212@gmail.com>
2024-03-06 22:44:07 +0900

    feature: 데이터베이스 초기화 코드 작성
    - 현재 demo단계에서 지원하는 데이터를 넣는 초기화 코드 작성

---
+++ src/main/java/capstone/examlab/exams/runner/DatabaseInitializer.java
@@ -0,0 +1,45 @@
+package capstone.examlab.exams.runner;
+
+import capstone.examlab.exams.entity.Exam;
+import capstone.examlab.exams.entity.SubExam;
+import capstone.examlab.exams.repository.ExamRepository;
+import capstone.examlab.exams.repository.SubExamRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class DatabaseInitializer implements ApplicationRunner {
+
+    private final ExamRepository examRepository;
+    private final SubExamRepository subExamRepository;
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // 데이터 베이스 초기화
+        examRepository.deleteAllInBatch();
+        subExamRepository.deleteAllInBatch();
+
+        // 시험 데이터 생성
+        Exam drivingLicenseExam = new Exam();
+        drivingLicenseExam.setExamTitle("운전면허");
+
+        drivingLicenseExam = examRepository.save(drivingLicenseExam);
+
+        // 부시험 데이터 생성
+        SubExam subExam = new SubExam();
+        subExam.setSubExamTitle("1종, 2종");
+        subExam.setTypes(Map.of("tags", List.of("상황", "표지")));
+        subExam = subExamRepository.save(subExam);
+
+        // 시험과 부시험 관계
+        drivingLicenseExam.getSubExams().add(subExam);
+        subExam.setExam(drivingLicenseExam);
+
+        examRepository.save(drivingLicenseExam);
+    }
+}

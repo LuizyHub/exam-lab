7d7871574b5be30aa7b0e8c0dba16524c79a0d8c
luizy <luizy991212@gmail.com>
2024-03-06 22:42:08 +0900

    feature: Entity 변경
    - 데이터베이스에 맞는 Entity 설계
    - 관계형 데이터베이스에서 Map 형태의 자료구조를 다루기가 힘들어서 우회해서 구현
    - 데이터베이스 종류를 다시 고민할 필요가 있어보임
    - 여러 구현 방법중, Entity의 원형을 최대한 유지하면서 이전할 수 있는 방법으로 설계

--- src/main/java/capstone/examlab/exams/repository/ExamDetailRepository.java
+++
@@ -1,8 +0,0 @@
-package capstone.examlab.exams.repository;
-
-import capstone.examlab.exams.entity.ExamDetailEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExamDetailRepository extends JpaRepository<ExamDetailEntity, Integer> {
-    ExamDetailEntity findByExamTitle(String testExam);
-}

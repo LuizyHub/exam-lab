7d19fa7e14de08f16ea8a2b6bfc6a332ec35530d
luizy <luizy991212@gmail.com>
2024-03-08 02:34:28 +0900

    feature: ExamService 구현
    - Exam Service 구현
    - Dto 명세에 맞게 수정

--- src/main/java/capstone/examlab/exams/dto/ExamDetail.java
+++ src/main/java/capstone/examlab/exams/dto/ExamDetail.java
@@ -1,13 +1,15 @@
 package capstone.examlab.exams.dto;
 
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Builder
 @Data
 public class ExamDetail {
-    private String title;
+    private String examTitle;
     private List<SubExamDetail> subExams = new ArrayList<>();
 }
--- src/main/java/capstone/examlab/exams/dto/ExamType.java
+++ src/main/java/capstone/examlab/exams/dto/ExamType.java
@@ -1,6 +1,10 @@
 package capstone.examlab.exams.dto;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class ExamType extends HashMap<String, Object> {
+    public ExamType(Map<? extends String, ?> m) {
+        super(m);
+    }
 }

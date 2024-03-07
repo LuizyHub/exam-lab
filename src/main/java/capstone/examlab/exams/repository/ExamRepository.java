82aab317dfe8db29201212c77b529aaa5d33d672
luizy <luizy991212@gmail.com>
2024-03-08 02:32:50 +0900

    feature: EntityRepository PK type 변경
    - ID Integer -> Long

--- src/main/java/capstone/examlab/exams/repository/ExamRepository.java
+++ src/main/java/capstone/examlab/exams/repository/ExamRepository.java
@@ -3,5 +3,5 @@ package capstone.examlab.exams.repository;
 import capstone.examlab.exams.entity.Exam;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExamRepository extends JpaRepository<Exam, Integer> {
+public interface ExamRepository extends JpaRepository<Exam, Long> {
 }

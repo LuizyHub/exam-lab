e6628c9e07e27f25939742332a73ddacfeb0f79d
4BFC <4bee.code@gmail.com>
2024-03-12 13:59:03 +0900

    refactor: change fetch to axios and add commentary rendering

--- src/components/LabExam.js
+++ src/components/LabExam.js
@@ -1,30 +1,32 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import { Link } from "react-router-dom";
 import parse from 'html-react-parser';
 import "../css/style.css"
-// shuffleArray(Fisher-Yates) 알고리즘을 사용하여 배열을 섞는 함수
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
 
 export default function LabExam() {
   const [data, setData] = useState([]);
   const [limit, setLimit] = useState(5);
+  const [isCommentary, setIsCommentary] = useState(false); //답안지 상태관리
 
   useEffect(() => {
-    fetch('http://localhost:3001/sample')
-      .then(res => res.json())
-      .then((data) => {
-        setData(data.slice(0, limit));
+    axios.get('http://localhost:3001/sample')
+      .then(res => {
+        setData(res.data.slice(0, limit));
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
   }, [limit]);
+
+  // shuffleArray(Fisher-Yates) 알고리즘을 사용하여 배열을 섞는 함수
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
 
   // 문제를 섞는 함수를 호출하는 버튼 클릭 핸들러
   const handleShuffle = () => {
@@ -32,6 +34,9 @@ export default function LabExam() {
     setData([...shuffledData]); // 새로운 배열로 설정하여 React가 업데이트를 감지할 수 있게 함
   };
 
+  const handleCommentary = () => {
+    setIsCommentary(!isCommentary)
+  }
 
   //클래스 추가
   // inside 이미지 파싱 후 렌더링 함수 수정
@@ -72,6 +77,7 @@ export default function LabExam() {
       <h2>Test</h2>
       {/* 문제 섞기 버튼 */}
       <button onClick={handleShuffle}>Shuffle</button>
+      <button onClick={handleCommentary}>Commentary</button>
       <select onChange={(e) => { setLimit(e.target.value); }}>
         <option value={5}>5</option>
         <option value={10}>10</option>
@@ -82,18 +88,21 @@ export default function LabExam() {
       <ol style={{ listStylePosition: 'inside' }}>
         {/* , listStyleType: 'none'   */}
         {data.map((item, index) => (
-          <li key={index} style={{ marginBottom: '70px', border: '1px solid black', width: '70%' }}>
-            {/* 질문 */}
-            <p>{parseImageTag(item.question, item.question_images_in)}</p>
-            {/* 이미지 렌더링 */}
-            <div style={{ width: '500px' }}>{renderImages(item.question_images_out)}</div>
-            {/* 4선지 */}
-            <ol style={{ listStyleType: 'none' }}>
-              {item.options.map((option, optionIndex) => (
-                <li key={optionIndex}>{option}</li>
-              ))}
-            </ol>
-          </li>
+          <div>
+            <li key={index} style={{ marginBottom: '70px', border: '1px solid black', width: '70%' }}>
+              {/* 질문 */}
+              <p>{parseImageTag(item.question, item.question_images_in)}</p>
+              {/* 이미지 렌더링 */}
+              <div style={{ width: '500px' }}>{renderImages(item.question_images_out)}</div>
+              {/* 4선지 */}
+              <ol style={{ listStyleType: 'none' }}>
+                {item.options.map((option, optionIndex) => (
+                  <li key={optionIndex}>{option}</li>
+                ))}
+              </ol>
+            </li>
+            <p style={{ display: isCommentary ? 'block' : 'none', marginBottom: '70px', border: '1px solid black', width: '70%' }}>해설 : {item.commentary}</p>
+          </div>
         ))}
       </ol>
     </div >

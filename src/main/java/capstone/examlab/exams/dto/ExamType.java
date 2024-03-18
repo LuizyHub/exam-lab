120ba7fdfd9ba1d5579cdfe6b03e82e05fde5225
luizy <luizy991212@gmail.com>
2024-03-18 23:17:35 +0900

    refactor: 부모타입 생성자 추가

--- src/main/java/capstone/examlab/exams/dto/ExamType.java
+++ src/main/java/capstone/examlab/exams/dto/ExamType.java
@@ -4,6 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ExamType extends HashMap<String, Object> {
+    public ExamType(int initialCapacity, float loadFactor) {
+        super(initialCapacity, loadFactor);
+    }
+
+    public ExamType(int initialCapacity) {
+        super(initialCapacity);
+    }
+
+    public ExamType() {
+    }
+
     public ExamType(Map<? extends String, ?> m) {
         super(m);
     }

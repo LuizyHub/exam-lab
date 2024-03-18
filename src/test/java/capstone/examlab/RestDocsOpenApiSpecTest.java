b4a78a212dea123cb1905cbd1abc03ce8fb9f142
luizy <luizy991212@gmail.com>
2024-03-18 23:21:08 +0900

    feature: API 문서화 테스트코드 작성
    - 테스트 코드 작성
    - 실제 코드가 동작하는 단계이므로 통합 테스트로 구성
    - 실제 동작을 검증하여 문서 신뢰성 보장

---
+++ src/test/java/capstone/examlab/RestDocsOpenApiSpecTest.java
@@ -0,0 +1,25 @@
+package capstone.examlab;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+@ExtendWith({RestDocumentationExtension.class})
+public abstract class RestDocsOpenApiSpecTest {
+
+    protected MockMvc mockMvc;
+
+    @BeforeEach
+    void setUpMockMvc(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))
+                .build();
+    }
+}

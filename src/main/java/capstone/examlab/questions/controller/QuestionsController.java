814f9f6999ee28ac3e871a7383f823e23ccda5ab
jisu-0305 <comjisu0311@naver.com>
2024-03-21 18:44:36 +0900

    feat: S3 커넥션 풀 및 이미지 생성, 제거 API 구현 #40

--- src/main/java/capstone/examlab/questions/controller/QuestionsController.java
+++ src/main/java/capstone/examlab/questions/controller/QuestionsController.java
@@ -1,5 +1,6 @@
 package capstone.examlab.questions.controller;
 
+import capstone.examlab.questions.dto.ImageSaveDto;
 import capstone.examlab.questions.dto.QuestionsList;
 import capstone.examlab.questions.dto.QuestionsOption;
 import capstone.examlab.questions.service.QuestionsService;
@@ -7,7 +8,10 @@ import capstone.examlab.valid.ValidExamId;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -19,5 +23,17 @@ public class QuestionsController {
     public QuestionsList getExamQuestions(@PathVariable @ValidExamId Long examId, @ModelAttribute @Valid QuestionsOption questionsOption) {
         log.info("questionOptionDto = {}", questionsOption);
         return questionsService.findByDriverLicenseQuestions(examId, questionsOption);
+    }
+
+    //이미지 관련 API
+    @PostMapping("image")
+    public List<String> saveImages(@ModelAttribute ImageSaveDto imageSaveDto) {
+        return questionsService.saveImages(imageSaveDto);
+    }
+
+    @DeleteMapping("image/delete/{imageName}")
+    public ResponseEntity<String> deleteImages(@PathVariable String imageName){
+        questionsService.deleteImages(imageName);
+        return ResponseEntity.ok("delete "+imageName);
     }
 }

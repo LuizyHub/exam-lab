3f28aa9278408a859659b9b056626b1c612b0e9c
luizy <luizy991212@gmail.com>
2024-03-26 21:32:12 +0900

    feature: 세션 적용
    - @Login 어노테이션 ArgumentResolver로 도입
    - Api와 Controller 분리
    - 안쓰는 import 정리
    - 템플릿 엔진 refactor

--- src/main/java/capstone/examlab/user/api/UserApi.java
+++ src/main/java/capstone/examlab/user/api/UserApi.java
@@ -1,61 +1,36 @@
 package capstone.examlab.user.api;
 
+import capstone.examlab.user.argumentresolver.Login;
 import capstone.examlab.user.domain.User;
-import capstone.examlab.user.dto.LoginDto;
-import capstone.examlab.user.dto.UserAddDto;
-import capstone.examlab.user.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.Valid;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-
 @Slf4j
-@Controller
+@RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserApi {
 
-    private final ObjectProvider<User> userProvider;
-
-    private final UserRepository userRepository;
-
-    @PostMapping("/add")
-    public String add(@Validated @ModelAttribute("user") UserAddDto userAddDto, BindingResult bindingResult) {
-
-        if (bindingResult.hasErrors()) {
-            return "users/addUserForm";
+    @GetMapping("/status")
+    public String status(@Login User user, HttpServletResponse response) {
+        if (user == null) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return "fail";
         }
-
-        log.info("user={}", userAddDto);
-        User user = userProvider.getObject();
-
-        user.setUserId(userAddDto.getUserId());
-        user.setName(userAddDto.getName());
-        user.setPassword(userAddDto.getPassword());
-
-        user = userRepository.save(user);
-        log.info("user={}", user);
-        log.info("userId={}", user.getId());
-
-        return "redirect:/api/v1/users/login";
+        return user.getName();
     }
 
-    @PostMapping("/login")
-    public String login(@Validated @ModelAttribute LoginDto loginDto, BindingResult bindingResult,
-                        @RequestParam(defaultValue = "/") String redirectURL) throws IOException {
-        if (bindingResult.hasErrors()) {
-            return "users/loginForm";
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
         }
-
-        log.info("loginDto={}", loginDto);
-
-        return "redirect:" + redirectURL;
+        return "ok";
     }
+
 }

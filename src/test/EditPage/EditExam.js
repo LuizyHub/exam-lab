85f70fb003c2a855294809cdfc53e30841d23652
4Bee <4bee.code@gmail.com>
2024-03-27 14:42:00 +0900

    feat: implement image size control

--- src/test/EditPage/EditExam.js
+++ src/test/EditPage/EditExam.js
@@ -1,98 +1,169 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { isElement } from 'react-dom/test-utils';
 
 export default function EditExam() {
   const [content, setContent] = useState('');
-  const [imageFiles, setImageFiles] = useState([]); // 이미지 파일들을 저장할 배열 상태
-  const [imageUrl, setImageUrl] = useState([]);
-
+  const [contentType, setContentType] = useState('');
+  const [imageFiles, setImageFiles] = useState([]); // 이미지 파일들을 저장할 배열 상태-> 이부분 수정 필요 정확히 역할은?
+  const [imageUrl, setImageUrl] = useState([]); //이미지 url값을 저장.. 배열로
+  const [isImageSize, setImageSize] = useState(50); //이미지 크기 값 저장
   // 여기서 우리가 style로 직접 주게된다면 style들을 저장하거나 해당 요소들을 확인 할 수 있는 코드를 작성해야한다.
-  const imageSelectorRef = useRef(null); // 파일 선택 input 요소에 대한 참조
-  const editorRef = useRef(null); //정확히 useRef를 사용하는 이유를 모르겠다.
+  const imageSelectorRef = useRef(null); // 파일 선택 input 요소에 접근에 대한 참조
+  const editorRef = useRef(null); //이미지를 appendChild할 때 dom의 위치를 참조하기 위함
+  // 객체로 저장
+  const [editorState, setEditorState] = useState({
+    type: '',
+    content: '',
+    imageUrl: []
+  });
+
+  // const { contentOBJ, imageFilesOBJ, imageUrlOBJ } = editorState;
 
   //---------------------------------------------------------------------- Tool Click area
   const focusEditor = () => {
     editorRef.current.focus({ preventScroll: true });
   };
 
-  // execCommand를 기준으로 생성된 코드
+  //execCommand를 기준으로 생성된 코드
   function setStyle(style) {
     document.execCommand(style);
     focusEditor();
   }
 
   const handleToolClick = (e) => {
-    const element = e.currentTarget.value;
+    const element = e.currentTarget.name;
+    //객체 저장
     console.log(element);
     setStyle(element);
     focusEditor();
   }
+  //contentType
+  const handleContentType = (e) => { //여기서 setContentType이 적용이 되기 때문에 
+    //----------------------------------------------------------------------------------------------------------- content를 저장하는 부분
+    const contentType = e.currentTarget.value;
+    setContentType(contentType);
+    // setEditorState({ ...editorState, type: contentType });
+    // console.log(editorState);
+  }
 
-  const handleImgToolClick = () => {
+  const handleImgToolClick = () => {//imageSelectorRef와 유사하게 작명이 필요하다. 즉, ref를 통해서 동작하는 fn명은 유사할 필요가 있다.
     // 파일 선택 input을 클릭합니다.
     imageSelectorRef.current.click();
   }
 
+
   //---------------------------------------------------------------------- save data area
   //여기에 html로 저장이 될 수 있게 코드를 작성
-  const handleSave = () => {
+  //이 코드가 납득이 안된다..
+  const handleContent = () => {
+    //----------------------------------------------------------------------------------------------------------- content를 저장하는 부분
     const inputContent = editorRef.current.innerHTML; //여기에는 html의 입력란을 제외한 html이 값이 들어간다.
     setContent(inputContent);
-    console.log(`inputContent : ${inputContent}`); //inputContent는 일반 적으로 HTML을 제외하고 출력
-    // console.log(imageFiles)
-    console.log(imageUrl)
+    // setEditorState({ ...editorState, content: inputContent }); //객체에 배열값을 저장하는 코드
+    // console.log("Updated editorState:", editorState); // editorState 값을 확인
+    console.log(contentType, content, imageUrl);
   };
 
+  //어떤 동작을 해도 데이터를 저장하는 코드
+  useEffect(() => {
+    handleContent();
+  }, [content, contentType, imageUrl]);
+
   //---------------------------------------------------------------------- image uploading area
+
+
   // 에디터가 업데이트될 때 이미지 삽입
+  /** 
+  왜 useEffect를 사용해야 하는건가? -> 렌더링 문제 때문에 그렇다. 더 명확한 답이 필요하다.
   useEffect(() => {
     if (editorRef.current && imageFiles.length > 0) {
       const file = imageFiles[imageFiles.length - 1];
-      insertImageDate(file);
+      insertImageData(file, 300);//이미지 크기를 인자로 전달 그리고 조절
     }
-  }, [editorRef.current, imageFiles]);
-
-
+  }, [imageFiles]);
+*/
+  // 이미지 선택 핸들러
   const handleImageSelect = (e) => {
     const files = e.target.files;
     if (!!files && files.length > 0) {
-      const file = files[0];//?
-      // 이미지 파일 배열에 추가
-      setImageFiles([...imageFiles, file]);
+      const file = files[0];
+      // 이미지 크기 제한
+      const maxSizeInBytes = 1e6; // 1MB
+      if (file.size > maxSizeInBytes) {
+        alert('이미지 크기가 너무 큽니다. 1MB보다 작은 파일을 선택해주세요.');
+        return;
+      }
+      // 이미지 포맷 확인
+      if (!file.name.toLowerCase().endsWith('.png')) {
+        alert('이미지 형식은 PNG만 지원합니다.');
+        return;
+      }
 
-      handleSave();
+      // 이미지 파일을 읽어들임
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        const imageDataUrl = e.target.result;
+        //--------------------------------------------------------------------------------------------- 이미지 URL 배열에 추가
+        setImageUrl([...imageUrl, imageDataUrl]);
+        // 이미지 삽입
+        insertImageData(imageDataUrl);
+      };
+      reader.readAsDataURL(file);
     }
   };
 
-  //이미지를 삽입하고 렌더링하는 함수
-  const insertImageDate = (file) => {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      const imageDataUrl = e.target.result;
-      // 이미지를 삽입합니다.
-      const imgElement = document.createElement('img');
-      imgElement.src = imageDataUrl;
-      setImageUrl([...imageUrl, imgElement.src]);
-      // editorRef가 null이 아닌 경우에만 이미지를 삽입합니다.
-      if (editorRef.current) {
-        editorRef.current.appendChild(imgElement);
-      } else {
-        console.error("Editor reference is null.");
-      }
-    };
-    reader.readAsDataURL(file);
+  // 이미지 삽입 함수
+  const insertImageData = (imageDataUrl) => {
+    const imgElement = document.createElement('img');
+    imgElement.src = imageDataUrl;
+    // 이미지 크기를 조절
+    imgElement.style.maxWidth = '50%';
+    imgElement.style.height = 'auto';
+    // 에디터에 이미지 삽입
+    if (editorRef.current) {
+      editorRef.current.appendChild(imgElement);
+    } else {
+      console.error("Editor reference is null.");
+    }
+  };
+
+  //----------------------------------------------------클릭된 이미지 확인 하는 코드
+  const handleImageCheck = (imgElement, size) => {
+    // const queryImage = document.querySelector('img');
+    // console.log(queryImage);
+    //해당 이미지가 생성되어 있는 요소이기에 addEventlistener로 접근해야한다.
+    imgElement.addEventListener('click', () => {
+      console.log(imgElement);
+      let currentSize = parseInt(imgElement.style.maxWidth);
+      currentSize = size; // 예시로 10% 감소시킵니다.
+      imgElement.style.maxWidth = `${currentSize}%`;
+    });
   };
 
   return (
     <div>
       <h1>Test</h1>
       <div className="editor-menu" style={{ marginBottom: '10px' }}>
-        <button value='bold' onClick={handleToolClick} style={{ marginRight: '5px' }}><b>B</b></button>
-        <button value='italic' onClick={handleToolClick} style={{ marginRight: '5px' }}><i>I</i></button>
-        <button value='underline' onClick={handleToolClick} style={{ marginRight: '5px' }}><u>U</u></button>
-        <button value='strikeThrough' onClick={handleToolClick} style={{ marginRight: '5px' }}><s>S</s></button>
-        <button value='insertOrderedList' onClick={handleToolClick} style={{ marginRight: '5px' }}>OL</button>
-        <button value='insertUnorderedList' onClick={handleToolClick} style={{ marginRight: '5px' }}>UL</button>
+        <select value={contentType} onChange={handleContentType}>
+          <option value="">Type</option>
+          <option value="문제">문제</option>
+          <option value="이미지">이미지</option>
+          <option value="선택지">선택지</option>
+        </select>
+        <button name='bold' onClick={handleToolClick} style={{ marginRight: '5px' }}><b>B</b></button>
+        <button name='italic' onClick={handleToolClick} style={{ marginRight: '5px' }}><i>I</i></button>
+        <button name='underline' onClick={handleToolClick} style={{ marginRight: '5px' }}><u>U</u></button>
+        <button name='strikeThrough' onClick={handleToolClick} style={{ marginRight: '5px' }}><s>S</s></button>
         <button onClick={handleImgToolClick} style={{ marginRight: '5px' }}>IMG</button>
+
+        <input type='number' value={isImageSize} onChange={(e) => {
+          const Image = document.querySelector('img');
+          const value = parseInt(e.target.value); //이벤트로 가져온 value
+          setImageSize(value); //아직 딜레이가 있음 이걸 해결 해야함
+          console.log(value)
+          Image.style.maxWidth = `${isImageSize}%`;
+          console.log(Image);
+        }} style={{ width: '60px' }} />
       </div>
 
       <input
@@ -108,10 +179,10 @@ export default function EditExam() {
         contentEditable="true"
         dangerouslySetInnerHTML={{ __html: content }} //content HTML문자열을 일반 문자열로 렌더링
         ref={editorRef}
-        style={{ padding: '16px 24px', border: '1px solid #D6D6D6', borderRadius: '4px' }}
+        style={{ padding: '16px 24px', border: '1px solid #D6D6D6', borderRadius: '4px', width: '600px' }}
       />
+      <button onClick={handleContent}>저장</button>
 
-      <button onClick={handleSave}>저장</button>
       <div>
         <p>Content in useState(include HTML tag):</p>
         {content}
--- src/test/EditPage/EditExamPrototype.js
+++ src/test/EditPage/EditExamPrototype.js
@@ -2,88 +2,116 @@ import React, { useState, useEffect, useRef } from 'react';
 
 export default function EditExam() {
   const [content, setContent] = useState('');
-  const [isString, setString] = useState(''); //HTML로 받은 값을 문자열로 전환해서 저장하는 상태
   const [imageFiles, setImageFiles] = useState([]); // 이미지 파일들을 저장할 배열 상태
+  const [imageUrl, setImageUrl] = useState([]);
 
   // 여기서 우리가 style로 직접 주게된다면 style들을 저장하거나 해당 요소들을 확인 할 수 있는 코드를 작성해야한다.
   const imageSelectorRef = useRef(null); // 파일 선택 input 요소에 대한 참조
   const editorRef = useRef(null); //정확히 useRef를 사용하는 이유를 모르겠다.
-  // const editorRef = React.createRef();
 
-  //Document.execCommand()를 대체할 수 있는 코드를 찾아 수정해야한다.
-  //이미지 압축 라이브러리를 찾아봐야한다.
-  //이미지가 중복되는 경우 추가가 되지 않는다. 이에 맞는 로직을 새로 다시 구현해야한다.
+  // 객체로 저장
+  const [editorState, setEditorState] = useState({
+    content: '',
+    imageUrl: []
+  });
 
-  /**
-  // contentEditable 요소에서 내용이 변경될 때 호출되는 함수
-  const handleChange = (e) => {
-    // HTML 내용 저장
-    setHtmlContent(e.target.innerHTML);
-    // 인라인 스타일 저장
-    const computedStyle = window.getComputedStyle(e.target);
-    const styleObject = {
-      padding: computedStyle.padding,
-      border: computedStyle.border,
-      borderRadius: computedStyle.borderRadius,
-      // 필요한 다른 스타일 속성들도 필요에 따라 추가할 수 있습니다.
-    };
-    setInlineStyle(styleObject);
-  }; 
-  */
+  // const { contentOBJ, imageFilesOBJ, imageUrlOBJ } = editorState;
 
-  // 일반적인 target을 사용하면 e.target이 잘 감지되지 않을 때가 있다. 
-  // 이유는 virtualDOM에 의한 이벤트 버블링이나 캡처링 단계에서 이벤트 타겟이 예상과 다를 수 있다.
-  // 해결할 수 있는 방법을 여러가지이지만 currentTarge.value 를 사용했다. 
+  //---------------------------------------------------------------------- Tool Click area
+  const focusEditor = () => {
+    editorRef.current.focus({ preventScroll: true });
+  };
+
+  //execCommand를 기준으로 생성된 코드
+  function setStyle(style) {
+    document.execCommand(style);
+    focusEditor();
+  }
+
+  // function setStyle(e, command) {
+  //   const selection = document.getSelection();
+  //   if (!selection || selection.rangeCount === 0) return;
+
+  //   const range = selection.getRangeAt(0);
+  //   const selectedText = range.toString();
+
+  //   const span = document.createElement('span');
+
+
+  //   // 선택된 텍스트에 적용된 스타일을 확인하여 스타일을 토글합니다.
+  //   switch (command) {
+  //     case 'bold':
+  //       applyStyle('font-weight', 'bold');
+  //       break;
+  //     case 'italic':
+  //       applyStyle('font-style', 'italic');
+  //       break;
+  //     case 'underline':
+  //       applyStyle('text-decoration', 'underline');
+  //       break;
+  //     case 'strikeThrough':
+  //       applyStyle('text-decoration', 'line-through');
+  //       break;
+  //     case 'fontSize':
+  //       // 폰트 크기를 선택하는 옵션 값 가져오기
+  //       const fontSize = e.target.value;
+  //       // 선택된 값에 따라 폰트 크기 설정
+  //       applyStyle('font-size', `${fontSize}px`);
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  //   function applyStyle(styleProperty, styleValue) {
+  //     const span = document.createElement('span');
+  //     span.style[styleProperty] = styleValue;
+  //     range.surroundContents(span);
+  //   }
+
+  //   focusEditor();
+  // }
+
+
   const handleToolClick = (e) => {
-    const element = e.currentTarget.value;
+    const element = e.currentTarget.name;
+    //객체 저장
     console.log(element);
     setStyle(element);
     focusEditor();
   }
 
+  const handleImgToolClick = () => {
+    // 파일 선택 input을 클릭합니다.
+    imageSelectorRef.current.click();
+  }
+
+  //---------------------------------------------------------------------- save data area
   //여기에 html로 저장이 될 수 있게 코드를 작성
   const handleSave = () => {
-    const inputContent = editorRef.current.innerHTML; //여기에는 html 값이 들어간다.
-    const stringContent = editorRef.current.outerHTML;
+    const inputContent = editorRef.current.innerHTML; //여기에는 html의 입력란을 제외한 html이 값이 들어간다.
+    setEditorState({ ...editorState, content: inputContent }); //객체에 배열값을 저장하는 코드
     setContent(inputContent);
-    setString(stringContent);
-    // console.log(content); //일반 적으로 HTML을 제외하고 출력
-    console.log(`inputContent : ${inputContent}\nstringContent : ${stringContent}`); //inputContent는 일반 적으로 HTML을 제외하고 출력
-    /** 
-     * //아래와 같이 확인을 하면 DOM에 직접 적으로 접근을 하기 때문에 react에서 권장하는 방법이 아니다. 따라서 useRef를 통해서 
-     * 참조로 접근을 해야한다.
-    const elementBOM = document.querySelector('.editor');
-    console.log(elementBOM.innerHTML); 
-    */
-    console.log(inputContent) //HTML의 모든 코드 저장 //현재 저장이 가능
+    console.log(`inputContent : ${inputContent}`); //inputContent는 일반 적으로 HTML을 제외하고 출력
+    console.log("Updated editorState:", editorState); // editorState 값을 확인
+    // console.log(imageFiles)
+    console.log(imageFiles)
   };
 
-  // image uploading area
+  //---------------------------------------------------------------------- image uploading area
   // 에디터가 업데이트될 때 이미지 삽입
   useEffect(() => {
     if (editorRef.current && imageFiles.length > 0) {
       const file = imageFiles[imageFiles.length - 1];
       insertImageDate(file);
-      console.log(imageFiles);
     }
   }, [editorRef.current, imageFiles]);
 
-  const handleUploadImageClick = () => {
-    // 파일 선택 input을 클릭합니다.
-    imageSelectorRef.current.click();
-  }
 
   const handleImageSelect = (e) => {
     const files = e.target.files;
     if (!!files && files.length > 0) {
-      const file = files[0];
+      const file = files[0];//?
       // 이미지 파일 배열에 추가
       setImageFiles([...imageFiles, file]);
-      // 이미지 파일 삽입 함수 호출
-      // insertImageDate(file);
-      // 이미지가 삽입된 후에 handleSave 호출 -> 
-      //이부분을 통해서 값이 저정됨을 알 수 있다. 
-      //하지만 값의 저장이 이미지를 로드해서 값이 저장된다는 단점이 있다 이를 해결 하기 위해선 어떻게 해야할까?
       handleSave();
     }
   };
@@ -96,6 +124,14 @@ export default function EditExam() {
       // 이미지를 삽입합니다.
       const imgElement = document.createElement('img');
       imgElement.src = imageDataUrl;
+      // const imageWrapper = document.createElement('span');
+      // imageWrapper.appendChild(imgElement);
+      imgElement.style.cssFloat = 'left';
+      // 이미지 컨테이너의 스타일을 설정하여 정렬을 조절합니다.
+      // imageContainer.style.textAlign = 'justify';
+      // imageContainer.style.float = 'left';
+      setImageUrl([...imageUrl, imgElement.src]);
+      setEditorState({ ...editorState, imageUrl: [...imageUrl, imgElement.src] });
       // editorRef가 null이 아닌 경우에만 이미지를 삽입합니다.
       if (editorRef.current) {
         editorRef.current.appendChild(imgElement);
@@ -106,52 +142,23 @@ export default function EditExam() {
     reader.readAsDataURL(file);
   };
 
-  const focusEditor = () => {
-    editorRef.current.focus({ preventScroll: true });
-  };
-
-  /* 아직 각 스타일을 적용 미적용이 불가능 한다. */
-  function setStyle(command) {
-    // CSS 스타일을 직접 설정하도록 변경
-    const editor = editorRef.current;
-    switch (command) {
-      case 'bold':
-        editor.style.fontWeight = 'bold';
-        break;
-      case 'italic':
-        editor.style.fontStyle = 'italic';
-        break;
-      case 'underline':
-        editor.style.textDecoration = 'underline';
-        break;
-      case 'strikeThrough':
-        document.execCommand('strikeThrough');
-        break;
-      case 'insertOrderedList':
-        document.execCommand('insertOrderedList');
-        break;
-      case 'insertUnorderedList':
-        document.execCommand('insertUnorderedList');
-        break;
-      default:
-        break;
-    }
-    focusEditor();
-  }
-
   return (
     <div>
       <h1>Test</h1>
       <div className="editor-menu" style={{ marginBottom: '10px' }}>
-        <button value='bold' onClick={handleToolClick} style={{ marginRight: '5px' }}><b>B</b></button>
-        <button value='italic' onClick={handleToolClick} style={{ marginRight: '5px' }}><i>I</i></button>
-        <button value='underline' onClick={handleToolClick} style={{ marginRight: '5px' }}><u>U</u></button>
-        <button value='strikeThrough' onClick={handleToolClick} style={{ marginRight: '5px' }}><s>S</s></button>
-        <button value='insertOrderedList' onClick={handleToolClick} style={{ marginRight: '5px' }}>OL</button>
-        <button value='insertUnorderedList' onClick={handleToolClick} style={{ marginRight: '5px' }}>UL</button>
-        <button onClick={handleUploadImageClick} style={{ marginRight: '5px' }}>IMG</button>
+        <button name='bold' onClick={handleToolClick} style={{ marginRight: '5px' }}><b>B</b></button>
+        <button name='italic' onClick={handleToolClick} style={{ marginRight: '5px' }}><i>I</i></button>
+        <button name='underline' onClick={handleToolClick} style={{ marginRight: '5px' }}><u>U</u></button>
+        <button name='strikeThrough' onClick={handleToolClick} style={{ marginRight: '5px' }}><s>S</s></button>
+        <button onClick={handleImgToolClick} style={{ marginRight: '5px' }}>IMG</button>
+        <select name='fontSize' onChange={handleToolClick} style={{ marginRight: '5px' }}>
+          <option name='fontSize' value="10">10px</option>
+          <option name='fontSize' value="12">12px</option>
+          <option name='fontSize' value="14">14px</option>
+          <option name='fontSize' value="16">16px</option>
+        </select>
       </div>
-      {/* 파일을 */}
+
       <input
         type="file"
         accept="image/*"
@@ -159,18 +166,19 @@ export default function EditExam() {
         ref={imageSelectorRef}
         onChange={handleImageSelect}
       />
+
       <div
         className="editor"
         contentEditable="true"
-        dangerouslySetInnerHTML={{ __html: content }}
-        // onChange={handleChange} // 내용이 변경될 때 handleChange 함수 호출
+        dangerouslySetInnerHTML={{ __html: content }} //content HTML문자열을 일반 문자열로 렌더링
         ref={editorRef}
         style={{ padding: '16px 24px', border: '1px solid #D6D6D6', borderRadius: '4px' }}
       />
       <button onClick={handleSave}>저장</button>
+
       <div>
         <p>Content in useState(include HTML tag):</p>
-        {isString}
+        {content}
       </div>
     </div>
   );

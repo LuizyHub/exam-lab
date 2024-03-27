2f0ac9c9f9be71daf09461ca1be94a1a2267cb12
jisu-0305 <comjisu0311@naver.com>
2024-03-27 16:59:24 +0900

    feat: QuestionsCreate Image부분 API 구현 #40

--- src/main/java/capstone/examlab/questions/repository/QuestionsRepository.java
+++ src/main/java/capstone/examlab/questions/repository/QuestionsRepository.java
@@ -3,6 +3,6 @@ package capstone.examlab.questions.repository;
 import capstone.examlab.questions.entity.QuestionEntity;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-public interface questionsRepository extends ElasticsearchRepository<QuestionEntity, String> {
+public interface QuestionsRepository extends ElasticsearchRepository<QuestionEntity, String> {
 
 }

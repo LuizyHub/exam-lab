73826577cb8b8db933984763283c3698d39da235
jisu-0305 <comjisu0311@naver.com>
2024-03-27 17:05:21 +0900

    feat: Questions Read API 구현#40

--- src/main/java/capstone/examlab/questions/service/QuestionsService.java
+++ src/main/java/capstone/examlab/questions/service/QuestionsService.java
@@ -2,6 +2,8 @@ package capstone.examlab.questions.service;
 
 import capstone.examlab.image.dto.ImagesUploadInfo;
 import capstone.examlab.questions.dto.ImageSave;
+import capstone.examlab.questions.dto.QuestionsList;
+import capstone.examlab.questions.dto.search.QuestionsSearch;
 import capstone.examlab.questions.dto.upload.QuestionsUpload;
 
 import java.util.List;
@@ -11,5 +13,7 @@ public interface QuestionsService {
    void addQuestionsByExamId(Long examId, QuestionsUpload questionsUpload);
 
     boolean addImageInQuestions(ImagesUploadInfo imagesUploadInfo);
-    //QuestionsList findByDriverLicenseQuestions(Long examId, QuestionsSearch questionsSearch);
+
+    QuestionsList searchFromQuestions(Long examId, QuestionsSearch questionsSearch);
+
 }

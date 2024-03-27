4707737e04814e68ee5cde3cd3891599e57b838e
jisu-0305 <comjisu0311@naver.com>
2024-03-27 16:59:24 +0900

    feat: QuestionsCreate Image부분 API 구현 #40

--- src/main/java/capstone/examlab/questions/service/QuestionsServiceImpl.java
+++ src/main/java/capstone/examlab/questions/service/QuestionsServiceImpl.java
@@ -1,21 +1,31 @@
 package capstone.examlab.questions.service;
 
+import capstone.examlab.image.dto.ImagesUploadInfo;
 import capstone.examlab.image.service.ImageService;
-import capstone.examlab.questions.dto.ImageSave;
+import capstone.examlab.questions.dto.*;
 import capstone.examlab.questions.dto.upload.QuestionUpload;
 import capstone.examlab.questions.dto.upload.QuestionsUpload;
 import capstone.examlab.questions.entity.QuestionEntity;
-import capstone.examlab.questions.repository.questionsRepository;
+import capstone.examlab.questions.repository.BoolQueryBuilder;
+import capstone.examlab.questions.repository.QuestionsRepository;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.client.elc.NativeQuery;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class QuestionsServiceImpl implements QuestionsService {
-    private final questionsRepository questionsRepository;
+    private final QuestionsRepository questionsRepository;
     private final ImageService imageService;
     //Create 로직
     @Override
@@ -44,15 +54,103 @@ public class QuestionsServiceImpl implements QuestionsService {
         }
     }
 
-    //Image관련 Service로직은 ImageService에 위임 - 추후 문제 데이터 추가시에 내용도 추가됨에 따라 QuestionsService 로직이 담당하는 부분이 많을 것으로 예상 -> 분산필요
     @Override
-    public List<String> saveImages(ImageSave imageSave){
-        if (imageSave.getImages() == null) return null;
-        return imageService.saveImagesInS3(imageSave);
+    public boolean addImageInQuestions(ImagesUploadInfo imagesUploadInfo) {
+        int size = imagesUploadInfo.getImageFiles().size();
+        int uuidSize = imagesUploadInfo.getQuestionsUuid().size();
+        int descriptionSize = imagesUploadInfo.getDescriptions().size();
+        int attributeSize = imagesUploadInfo.getAttributes().size();
+        int imageTypeSize = imagesUploadInfo.getImagesType().size();
+        if(size == 0) return false;
+
+        String beforeUuid = "";
+        int questionImagesInCount = 0;
+        int questionImagesOutCount = 0;
+        int commentaryImagesInCount = 0;
+        int commentaryImagesOutCount = 0;
+
+        for (int i = 0; i < size; i++) {
+            //이미지 넣는 로직
+            MultipartFile imageFile = imagesUploadInfo.getImageFiles().get(i);
+            String uuid = "";
+            if(uuidSize>i) uuid = imagesUploadInfo.getQuestionsUuid().get(i);
+            String imageDescription = "";
+            if(descriptionSize>i) imageDescription = imagesUploadInfo.getDescriptions().get(i);
+            String imageAttribute = "";
+            if(attributeSize>i) imageAttribute = imagesUploadInfo.getAttributes().get(i);
+            String imageType = "";
+            if(imageTypeSize>i) imageType = String.valueOf(imagesUploadInfo.getImagesType().get(i));
+
+            if(!beforeUuid.equals(uuid)){
+                beforeUuid = uuid;
+                questionImagesInCount = 0;
+                questionImagesOutCount = 0;
+                commentaryImagesInCount = 0;
+                commentaryImagesOutCount = 0;
+            }
+
+            Optional<QuestionEntity> optionalQuestion = questionsRepository.findById(uuid);
+            String imageUrl ="";
+            ImageDto imageDto;
+            switch (imageType) {
+                case "questionImagesIn":
+                    questionImagesInCount++;
+                    imageUrl = imageService.saveImageInS3(imageFile, questionImagesInCount);
+                    log.info("imageUrl: "+imageUrl);
+                    imageDto = ImageDto.builder()
+                            .url(imageUrl)
+                            .description(imageDescription)
+                            .attribute(imageAttribute)
+                            .build();
+                    optionalQuestion.ifPresent(question -> {
+                        question.getQuestionImagesIn().add(imageDto);
+                        questionsRepository.save(question); // 수정된 질문 저장
+                    });
+                    break;
+                case "questionImagesOut":
+                    questionImagesOutCount++;
+                    imageUrl = imageService.saveImageInS3(imageFile, questionImagesOutCount);
+                    imageDto = ImageDto.builder()
+                            .url(imageUrl)
+                            .description(imageDescription)
+                            .attribute(imageAttribute)
+                            .build();
+                    optionalQuestion.ifPresent(question -> {
+                        question.getQuestionImagesOut().add(imageDto);
+                        questionsRepository.save(question); // 수정된 질문 저장
+                    });
+                    break;
+                case "commentaryImagesIn":
+                    commentaryImagesInCount++;
+                    imageUrl = imageService.saveImageInS3(imageFile, commentaryImagesInCount);
+                    imageDto = ImageDto.builder()
+                            .url(imageUrl)
+                            .description(imageDescription)
+                            .attribute(imageAttribute)
+                            .build();
+                    optionalQuestion.ifPresent(question -> {
+                        question.getCommentaryImagesIn().add(imageDto);
+                        questionsRepository.save(question); // 수정된 질문 저장
+                    });
+                    break;
+                case "commentaryImagesOut":
+                    commentaryImagesOutCount++;
+                    imageUrl = imageService.saveImageInS3(imageFile, commentaryImagesOutCount);
+                    imageDto = ImageDto.builder()
+                            .url(imageUrl)
+                            .description(imageDescription)
+                            .attribute(imageAttribute)
+                            .build();
+                    optionalQuestion.ifPresent(question -> {
+                        question.getCommentaryImagesOut().add(imageDto);
+                        questionsRepository.save(question); // 수정된 질문 저장
+                    });
+                    break;
+                default:
+                    return false;
+            }
+        }
+        return true;
     }
 
-    @Override
-    public void deleteImages(String imageName){
-        imageService.deleteImageInFolder(imageName);
-    }
 }

fe3d704724cc5560ac9177995293dbfdd19f12c3
jisu-0305 <comjisu0311@naver.com>
2024-03-28 10:12:24 +0900

    feat: Questions Delete API 구현 #40

--- src/main/java/capstone/examlab/questions/repository/QuestionsRepository.java
+++ src/main/java/capstone/examlab/questions/repository/QuestionsRepository.java
@@ -3,6 +3,11 @@ package capstone.examlab.questions.repository;
 import capstone.examlab.questions.entity.QuestionEntity;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.List;
+
 public interface QuestionsRepository extends ElasticsearchRepository<QuestionEntity, String> {
 
+    List<QuestionEntity> findByExamId(Long examId);
+
+    void deleteByExamId(Long examId);
 }

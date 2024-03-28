dfcee98256400fa3c05a6932027ec035903f7bbd
jisu-0305 <comjisu0311@naver.com>
2024-03-28 10:55:37 +0900

    refactor: 분리해서 구현했던 기본 Questions data add API 구현 #40

---
+++ src/main/java/capstone/examlab/questions/datainput/QuestionsDataController.java
@@ -0,0 +1,27 @@
+package capstone.examlab.questions.datainput;
+
+
+import capstone.examlab.questions.entity.QuestionEntity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("waring")
+public class QuestionsDataController {
+    private final QuestionsDataService questionsDataService;
+    //'기존 문제' 저장 로직
+    @PostMapping("{examId}/questions/save")
+    public ResponseEntity<String> addOriginalQuestions(@PathVariable Long examId, @RequestBody List<QuestionEntity> questionEntities) {
+        for (QuestionEntity questionEntity : questionEntities) {
+            questionEntity.setExamId(examId);
+        }
+        questionsDataService.saveQuestions(questionEntities);
+        return ResponseEntity.ok("data add success");
+    }
+}

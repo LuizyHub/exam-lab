bd23419c6c479d341da92ef0897f11ee7e8410f1
jisu-0305 <comjisu0311@naver.com>
2024-03-28 19:22:49 +0900

    refactor: 문제들로 데이터 전송 -> 문제 각각 데이터 전송으로 방식 변경 #40

--- src/main/java/capstone/examlab/questions/dto/upload/QuestionUpload.java
+++ src/main/java/capstone/examlab/questions/dto/upload/QuestionUpload.java
@@ -1,19 +1,28 @@
 package capstone.examlab.questions.dto.upload;
 
+import capstone.examlab.image.dto.ImagesUploadInfo;
+import capstone.examlab.questions.dto.ImageDto;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Map;
 
+/*
+    depricated: 문제들 단위가 아닌 문제 단위로 api호출해서 업로드하는 방식을 채택 -> 따라서 List형이 불필요해짐
+ */
+
+
 @Data
 @Builder
 public class QuestionUpload {
-    private String uuid;
+
+/*    private String uuid;
     private String type;
     private String question;
     private List<String> options;
     private List<Integer> answers;
     private String commentary;
-    private Map<String, List<String>> tagsMap;
+    private Map<String, List<String>> tagsMap;*/
 }

8920da2f64a6978316d33572b6780991b205fb9b
luizy <luizy991212@gmail.com>
2024-04-02 00:27:29 +0900

    feature: 데이터베이스 추상화

--- src/main/java/capstone/examlab/exams/config/ExamConfig.java
+++ src/main/java/capstone/examlab/exams/config/ExamConfig.java
@@ -2,18 +2,28 @@ package capstone.examlab.exams.config;
 
 import capstone.examlab.config.profile.ProfileJPA;
 import capstone.examlab.exams.domain.Exam;
+import capstone.examlab.exams.domain.ExamDoc;
+import capstone.examlab.exams.domain.ExamEntity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class ExamConfig {
 
     @Bean
-    @ProfileJPA
+    @Profile("jpa")
     @Scope("prototype")
-    public Exam exam() {
-        return new Exam();
+    public Exam examEntity() {
+        return new ExamEntity();
+    }
+
+    @Bean
+    @Profile("mongo")
+    @Scope("prototype")
+    public Exam examDoc() {
+        return new ExamDoc();
     }
 
 }

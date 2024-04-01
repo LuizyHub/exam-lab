784c760b8836d73a2bd738e791bc3b003537e960
luizy <luizy991212@gmail.com>
2024-04-02 00:26:34 +0900

    feature: Exam CRUD 구현

--- src/main/java/capstone/examlab/exams/domain/Exam.java
+++ src/main/java/capstone/examlab/exams/domain/Exam.java
@@ -1,63 +1,28 @@
 package capstone.examlab.exams.domain;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.persistence.*;
-import lombok.AccessLevel;
+import capstone.examlab.users.domain.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 
-@Slf4j
 @Data
-@Entity
-public class Exam {
+@MappedSuperclass
+public abstract class Exam {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long examId;
+    protected String examTitle;
 
-    private String examTitle;
+    abstract public Long getExamId();
 
-    @Getter(AccessLevel.NONE) // 이 필드에 대한 getter는 생성하지 않음
-    @Setter(AccessLevel.NONE) // 이 필드에 대한 setter는 생성하지 않음
-    @Lob
-    private String typesMetadata; // JSON 문자열을 저장하는 필드
+    abstract public void setExamId(Long examId);
 
-    @Transient
-    private Map<String, List<String>> types; // DB에 저장되지 않는 필드
+    abstract public Map<String, List<String>> getTypes();
 
-    // ObjectMapper는 JSON 변환을 담당하는 클래스입니다.
-    // Spring Boot 환경에서는 Bean으로 등록된 ObjectMapper 인스턴스를 주입받아 사용할 수 있습니다.
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    abstract public void setTypes(Map<String, List<String>> types);
 
-    public Map<String, List<String>> getTypes() {
-        if (this.typesMetadata == null || this.typesMetadata.isEmpty()) {
-            return new HashMap<>();
-        }
-        try {
-            // metadata 문자열을 Map<String, List<String>>로 변환합니다.
-            this.types = objectMapper.readValue(this.typesMetadata, new TypeReference<Map<String, List<String>>>() {});
-        } catch (IOException e) {
-            e.printStackTrace();
-            log.error("JSON 문자열을 Map으로 변환하는 중 에러가 발생했습니다. = {}", this.typesMetadata);
-            // 변환 중 에러가 발생하면 빈 Map을 반환
-            return new HashMap<>();
-        }
-        return this.types;
-    }
+    abstract public User getUser();
 
-    public void setTypes(Map<String, List<String>> types) {
-        this.types = types;
-        try {
-            // Map을 JSON 문자열로 변환하여 metadata에 저장합니다.
-            this.typesMetadata = objectMapper.writeValueAsString(types);
-        } catch (IOException e) {
-            log.error("Map을 JSON 문자열로 변환하는 중 에러가 발생했습니다. = {}", e.getMessage());
-        }
-    }
+    abstract public void setUser(User user);
 }
--- src/main/java/capstone/examlab/exams/service/ExamsService.java
+++ src/main/java/capstone/examlab/exams/service/ExamsService.java
@@ -1,7 +1,10 @@
 package capstone.examlab.exams.service;
 
+import capstone.examlab.exams.dto.ExamAddDto;
+import capstone.examlab.exams.dto.ExamPatchDto;
 import capstone.examlab.exams.dto.ExamTypeDto;
 import capstone.examlab.exams.dto.ExamDto;
+import capstone.examlab.users.domain.User;
 
 import java.util.List;
 
@@ -10,4 +13,10 @@ public interface ExamsService {
     public ExamTypeDto getExamType(Long id);
 
     public List<ExamDto> getExamList();
+
+    Long createExam(ExamAddDto examAddDto, User user);
+
+    public void deleteExam(Long id, User user);
+
+    public void patchExam(Long id, ExamPatchDto examAddDto, User user);
 }

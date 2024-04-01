a69a317d459fee935d68f87b499699b736657cd6
jisu-0305 <comjisu0311@naver.com>
2024-04-01 10:11:18 +0900

    refactor: Update, Delete 처리방식 변경: 데이터 개별 처리 API로 변경 #40

--- src/main/java/capstone/examlab/questions/controller/QuestionsController.java
+++ src/main/java/capstone/examlab/questions/controller/QuestionsController.java
@@ -1,14 +1,10 @@
 package capstone.examlab.questions.controller;
 
-import capstone.examlab.image.dto.ImagesUploadInfo;
 import capstone.examlab.questions.dto.QuestionsList;
 import capstone.examlab.questions.dto.search.QuestionsSearch;
-import capstone.examlab.questions.dto.update.QuestionsUpdateDto;
-import capstone.examlab.questions.dto.upload.QuestionUpload;
+import capstone.examlab.questions.dto.update.QuestionUpdateDto;
 import capstone.examlab.questions.dto.upload.QuestionUploadInfo;
-import capstone.examlab.questions.dto.upload.QuestionsUpload;
 import capstone.examlab.questions.service.QuestionsService;
-import capstone.examlab.valid.ValidExamId;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -26,7 +22,8 @@ public class QuestionsController {
 
     //Create API, '신규 문제' 저장 API
     @PostMapping("{examId}/questions")
-    public ResponseEntity<String> addQuestionsByExamId(@PathVariable Long examId, @RequestPart QuestionUploadInfo questionUploadInfo, @RequestPart List<MultipartFile> questionImagesIn, @RequestPart List<MultipartFile> questionImagesOut, @RequestPart List<MultipartFile> commentaryImagesIn, @RequestPart List<MultipartFile> commentaryImagesOut) {
+    public ResponseEntity<String> addQuestionsByExamId(@PathVariable Long examId, @RequestPart QuestionUploadInfo questionUploadInfo, @RequestPart(name = "questionImagesIn", required = false) List<MultipartFile> questionImagesIn,
+           @RequestPart(name = "questionImagesOut", required = false) List<MultipartFile> questionImagesOut, @RequestPart(name = "commentaryImagesIn", required = false) List<MultipartFile> commentaryImagesIn, @RequestPart(name = "commentaryImagesOut", required = false) List<MultipartFile> commentaryImagesOut) {
         boolean saved = questionsService.addQuestionsByExamId(examId, questionUploadInfo,questionImagesIn, questionImagesOut, commentaryImagesIn, commentaryImagesOut);
         if(saved){
             return ResponseEntity.ok("question add suucess");
@@ -64,11 +61,11 @@ public class QuestionsController {
 
     //Update API
     @PostMapping("/questions/update")
-    public ResponseEntity<String> updateQuestions(@RequestBody QuestionsUpdateDto questionsUpdateDto){
+    public ResponseEntity<String> updateQuestions(@RequestBody QuestionUpdateDto questionUpdateDto){
   /*      for (QuestionUpdateDto questionUpdateDto : questionsUpdateDto) {
             log.info(questionUpdateDto.toString());
         }*/
-        boolean updated = questionsService.updateQuestionsByUUID(questionsUpdateDto);
+        boolean updated = questionsService.updateQuestionsByUUID(questionUpdateDto);
         if(updated){
             return ResponseEntity.ok("data update success");
         }else{
@@ -89,9 +86,9 @@ public class QuestionsController {
     }
 
     //문제들(List<uuid>) API
-    @DeleteMapping("questions/uuid")
-    public ResponseEntity<String> deleteQuestionsByUUID(@RequestBody List<String> uuidList) {
-        boolean deleted = questionsService.deleteQuestionsByUuidList(uuidList);
+    @DeleteMapping("questions/{uuid}")
+    public ResponseEntity<String> deleteQuestionsByUUID(@PathVariable String uuid) {
+        boolean deleted = questionsService.deleteQuestionsByUuidList(uuid);
         if (deleted) {
             return ResponseEntity.ok("data delete success");
         } else {

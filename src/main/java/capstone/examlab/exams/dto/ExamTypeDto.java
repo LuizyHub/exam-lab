9e58272c977fab164ba581ec584b48aef8faa2e4
luizy <luizy991212@gmail.com>
2024-04-03 20:57:15 +0900

    refactor: pr내용 반영
    - 불필요한 주석 삭제
    - 알맞는 함수명 적용
    - 타입 수정

--- src/main/java/capstone/examlab/exams/dto/ExamTypeDto.java
+++ src/main/java/capstone/examlab/exams/dto/ExamTypeDto.java
@@ -1,9 +1,10 @@
 package capstone.examlab.exams.dto;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class ExamTypeDto extends HashMap<String, Object> {
+public class ExamTypeDto extends HashMap<String, List<String>> {
     public ExamTypeDto(int initialCapacity, float loadFactor) {
         super(initialCapacity, loadFactor);
     }
@@ -15,7 +16,7 @@ public class ExamTypeDto extends HashMap<String, Object> {
     public ExamTypeDto() {
     }
 
-    public ExamTypeDto(Map<? extends String, ?> m) {
+    public ExamTypeDto(Map<? extends String, ? extends List<String>> m) {
         super(m);
     }
 }

b1a3cb26c69083d1e1eb0d110b1ab369f168e965
jisu-0305 <comjisu0311@naver.com>
2024-04-05 14:08:42 +0900

    feat: RequestParam통해서 데이터 받기 #62

--- src/main/java/capstone/examlab/questions/controller/QuestionsController.java
+++ src/main/java/capstone/examlab/questions/controller/QuestionsController.java
@@ -12,7 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -24,30 +27,53 @@ public class QuestionsController {
     //Create API
     @PostMapping("{examId}/questions")
     public ResponseEntity<String> addQuestionsByExamId(@PathVariable @ValidExamId Long examId, @RequestPart QuestionUploadInfo questionUploadInfo, @RequestPart(name = "questionImagesIn", required = false) List<MultipartFile> questionImagesIn,
-           @RequestPart(name = "questionImagesOut", required = false) List<MultipartFile> questionImagesOut, @RequestPart(name = "commentaryImagesIn", required = false) List<MultipartFile> commentaryImagesIn, @RequestPart(name = "commentaryImagesOut", required = false) List<MultipartFile> commentaryImagesOut) {
-        boolean saved = questionsService.addQuestionsByExamId(examId, questionUploadInfo,questionImagesIn, questionImagesOut, commentaryImagesIn, commentaryImagesOut);
-        if(saved){
+                                                       @RequestPart(name = "questionImagesOut", required = false) List<MultipartFile> questionImagesOut, @RequestPart(name = "commentaryImagesIn", required = false) List<MultipartFile> commentaryImagesIn, @RequestPart(name = "commentaryImagesOut", required = false) List<MultipartFile> commentaryImagesOut) {
+        boolean saved = questionsService.addQuestionsByExamId(examId, questionUploadInfo, questionImagesIn, questionImagesOut, commentaryImagesIn, commentaryImagesOut);
+        if (saved) {
             return ResponseEntity.ok("question add suucess");
-        }
-        else{
+        } else {
             return ResponseEntity.badRequest().body("question add error");
         }
     }
 
     //Read API
     @GetMapping("{examId}/questions")
-    public QuestionsList selectQuestions(@PathVariable @ValidExamId Long examId, @RequestBody QuestionsSearch questionsSearch) {
+    public QuestionsList selectQuestions(@PathVariable @ValidExamId Long examId, @RequestParam Map<String, String> params) {
+        Map<String, List<String>> tagsMap = new HashMap<>();
+        List<String> includes = new ArrayList<>();
+        int count = 10;
+
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            if (key.startsWith("tagsMap_")) {
+                String[] tokens = key.split("_");
+                String category = tokens[1];
+                //키가 맵에 없으면 새로 생성, value값이 list타입이므로 새로 생성
+                tagsMap.computeIfAbsent(category, k -> new ArrayList<String>()).add(value);
+            } else if (key.equals("includes")) {
+                includes.add(value);
+            } else if (key.equals("count")) {
+                count = Integer.parseInt(value);
+            }
+        }
+
+        QuestionsSearch questionsSearch = QuestionsSearch.builder()
+                .tagsMap(tagsMap)
+                .count(count)
+                .includes(includes)
+                .build();
         log.info("questionOptionDto = {}", questionsSearch);
         return questionsService.searchFromQuestions(examId, questionsSearch);
     }
 
     //Update API
     @PutMapping("/questions")
-    public ResponseEntity<String> updateQuestions(@RequestBody QuestionUpdateDto questionUpdateDto){
+    public ResponseEntity<String> updateQuestions(@RequestBody QuestionUpdateDto questionUpdateDto) {
         boolean updated = questionsService.updateQuestionsByQuestionId(questionUpdateDto);
-        if(updated){
+        if (updated) {
             return ResponseEntity.ok("data update success");
-        }else{
+        } else {
             return ResponseEntity.badRequest().body("data update error");
         }
     }

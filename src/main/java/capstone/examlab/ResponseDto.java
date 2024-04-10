595df025e100d137165dc99dbc5cabb99c8eb04c
luizy <luizy991212@gmail.com>
2024-04-10 21:22:08 +0900

    feature: 공통 응답 class 생성 #65

---
+++ src/main/java/capstone/examlab/ResponseDto.java
@@ -0,0 +1,22 @@
+package capstone.examlab;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Getter
+@RequiredArgsConstructor
+public class ResponseDto {
+    private final Integer code;
+    private final String message;
+
+    public static ResponseDto of(Integer code, String message) {
+        return new ResponseDto(code, message);
+    }
+
+    public static final ResponseDto OK = new ResponseDto(200, "OK");
+    public static final ResponseDto CREATED = new ResponseDto(201, "CREATED");
+    public static final ResponseDto BAD_REQUEST = new ResponseDto(400, "BAD_REQUEST");
+    public static final ResponseDto UNAUTHORIZED = new ResponseDto(401, "UNAUTHORIZED");
+}

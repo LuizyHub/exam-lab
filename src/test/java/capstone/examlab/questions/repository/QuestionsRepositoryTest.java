9f31275917750c7d2bc92e4c3d4c13018d27f92f
jisu-0305 <comjisu0311@naver.com>
2024-04-16 02:10:21 +0900

    config: QuestionsRepositoryTest를 위한 기본 설정 #69

--- src/test/java/capstone/examlab/questions/repository/QuestionsRepositoryTest.java
+++ src/test/java/capstone/examlab/questions/repository/QuestionsRepositoryTest.java
@@ -1,54 +1,37 @@
 package capstone.examlab.questions.repository;
 
-import capstone.examlab.ExamLabApplication;
 import capstone.examlab.questions.dto.ImageDto;
+import capstone.examlab.questions.dto.QuestionsListDto;
+import capstone.examlab.questions.dto.search.QuestionsSearchDto;
+import capstone.examlab.questions.dto.upload.QuestionUploadInfo;
+
 import capstone.examlab.questions.entity.QuestionEntity;
 import capstone.examlab.questions.service.QuestionsService;
-
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.*;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.data.elasticsearch.DataElasticsearchTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
-import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.*;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataElasticsearchTest
 @Tag("db_test")
 @Testcontainers
-class QuestionsRepositoryTest {
- /*   @Container
-    private static final ElasticsearchContainer container = new ElasticsearchContainer(
-            DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch:8.11.3"))
-            .withPassword("elastic123")
-            .withReuse(true);
+public class QuestionsRepositoryTest {
+    private final static String IMAGE_NAME = "docker.elastic.co/elasticsearch/elasticsearch:7.17.5";
 
-    @Configuration
-    static class TestConfiguration extends ElasticsearchConfiguration {
-        @Override
-        public @NotNull ClientConfiguration clientConfiguration() {
+    @Container
+    @ServiceConnection
+    public static ElasticsearchContainer ES_CONTAINER = new ElasticsearchContainer(IMAGE_NAME);
 
-            return ClientConfiguration.builder()
-                    .connectedTo(container.getHttpHostAddress())
-                    .usingSsl(container.createSslContextFromCa())
-                    .withBasicAuth("elastic", "elastic123")
-                    .build();
-        }
-    }
-    @Mock
+    @Autowired
     private QuestionsRepository questionsRepository;
 
     @MockBean
@@ -57,148 +40,41 @@ class QuestionsRepositoryTest {
     @MockBean
     private BoolQueryBuilder boolQueryBuilder;
 
-    @MockBean
-    private ElasticsearchTemplate elasticsearchTemplate;
+    private final Long existExamId = 1L;
+
+    private final String questionUuid = "f8bd102d-c20a-4385-94f1-a4078843bg28";
+
+    @BeforeEach
+    public void setup(){
+        QuestionEntity question = QuestionEntity.builder()
+                .id(questionUuid)
+                .examId(existExamId)
+                .type("객관식")
+                .question("다음 중 총중량 1.5톤 피견인 승용자동차를 4.5톤 화물자동차로 견인하는 경우 필요한 운전면허에 해당하지 않은 것은?")
+                .options(List.of(
+                        "① 제1종 대형면허 및 소형견인차면허",
+                        "② 제1종 보통면허 및 대형견인차면허",
+                        "③ 제1종 보통면허 및 소형견인차면허",
+                        "④ 제2종 보통면허 및 대형견인차면허"))
+                .questionImagesIn(List.of(
+                        ImageDto.builder().url("url1").description("설명1").attribute("속성1").build()))
+                .questionImagesOut(List.of(
+                        ImageDto.builder().url("url2").description("설명2").attribute("속성2").build()))
+                .answers(List.of(4))
+                .commentary("도로교통법 시행규칙 별표18 총중량 750킬로그램을 초과하는 3톤 이하의 피견인 자동차를 견인하기 위해서는 견인 하는 자동차를 운전할 수 있는 면허와 소형견인차면허 또는 대형견인차면허를 가지고 있어야 한다.")
+                .commentaryImagesIn(List.of(
+                        ImageDto.builder().url("url3").description("설명3").attribute("속성3").build()))
+                .commentaryImagesOut(List.of(
+                        ImageDto.builder().url("url4").description("설명4").attribute("속성4").build()))
+                .tagsMap(Map.of("category", List.of("화물")))
+                .build();
+
+        System.out.println(questionsRepository.save(question).getId());
+    }
 
     @Test
-    void testRepository() {
-        ImageDto questionImagesOut = ImageDto.builder()
-                .url("https://examlab-image.s3.ap-northeast-2.amazonaws.com/driver_images/870.png")
-                .description("")
-                .attribute("examlab-image-right")
-                .build();
+    void testDatabaseIsRunning() {
+        assertThat(ES_CONTAINER.isRunning()).isTrue();
 
-        Map<String, List<String>> tagsMap = new HashMap<>();
-        tagsMap.put("category", List.of("법", "상황"));
-
-        QuestionEntity question = QuestionEntity.builder()
-                .id("4d51a532-3495-4fab-beef-27cc974c64b1")
-                .examId(1L)
-                .type("객관식")
-                .question("예시 문제1: 도로교통법령상, 포함된 단어 찾기 문제?")
-                .options(List.of(
-                        "① 규제표지이다.",
-                        "② 직진차량 우선표지이다.",
-                        "③ 좌합류 도로표지이다.",
-                        "④ 좌회전 금지표지이다."))
-                .answers(List.of(3))
-                .questionImagesOut(List.of(questionImagesOut))
-                .commentary("commentary 예시1")
-                .commentaryImagesIn(List.of())
-                .commentaryImagesOut(List.of())
-                .tagsMap(tagsMap)
-                .build();
-
-        questionsRepository.save(question);
-        //questionsService.deleteQuestionsByQuestionId(question.getId());
-        Optional<QuestionEntity> savedQuestion = questionsRepository.findById(question.getId());
-        assertThat(savedQuestion).isPresent();
-    }*/
-
-
-
-//
-//    @BeforeEach
-//    void addTestData() {
-//        List<QuestionEntity>questionEntities = new ArrayList<>();
-//        // 예시 1 생성
-//        ImageDto questionImagesOut = ImageDto.builder()
-//                .url("https://examlab-image.s3.ap-northeast-2.amazonaws.com/driver_images/870.png")
-//                .description("")
-//                .attribute("examlab-image-right")
-//                .build();
-//
-//        Map<String, List<String>> tagsMap = new HashMap<>();
-//        tagsMap.put("category", List.of("법", "상황"));
-//
-//        QuestionEntity question1 = QuestionEntity.builder()
-//                .id("4d51a532-3495-4fab-beef-27cc974c64b1")
-//                .examId(1L)
-//                .type("객관식")
-//                .question("예시 문제1: 도로교통법령상, 포함된 단어 찾기 문제?")
-//                .options(List.of(
-//                        "① 규제표지이다.",
-//                        "② 직진차량 우선표지이다.",
-//                        "③ 좌합류 도로표지이다.",
-//                        "④ 좌회전 금지표지이다."))
-//                .answers(List.of(3))
-//                .questionImagesOut(List.of(questionImagesOut))
-//                .commentary("commentary 예시1")
-//                .commentaryImagesIn(List.of())
-//                .commentaryImagesOut(List.of())
-//                .tagsMap(tagsMap)
-//                .build();
-//        questionEntities.add(question1);
-//        // 예시 2 생성
-//        ImageDto questionImagesOut2 = ImageDto.builder()
-//                .url("https://examlab-image.s3.ap-northeast-2.amazonaws.com/driver_images/872.png")
-//                .description("")
-//                .attribute("examlab-image-right")
-//                .build();
-//
-//        Map<String, List<String>> tagsMap2 = new HashMap<>();
-//        tagsMap2.put("category", List.of("법", "화물"));
-//        tagsMap2.put("year", List.of("20년도"));
-//
-//        QuestionEntity question2 = QuestionEntity.builder()
-//                .id("55cb3411-4b1d-4320-a46d-b8d36bb6b808")
-//                .examId(1L)
-//                .type("객관식")
-//                .question("예시 문제2 examLab화이팅 이 단어 포함되면 찾을 수 있는 로직은?")
-//                .options(List.of(
-//                        "① 보기1",
-//                        "② 보기2",
-//                        "③ 보기3",
-//                        "④ 보기4"))
-//                .answers(List.of(1))
-//                .questionImagesOut(List.of(questionImagesOut2))
-//                .commentary("commentary 예시2")
-//                .commentaryImagesIn(List.of())
-//                .commentaryImagesOut(List.of())
-//                .tagsMap(tagsMap2)
-//                .build();
-//        questionEntities.add(question2);
-//        questionsRepository.saveAll(questionEntities);
-//        System.out.println("Data size: " + questionsRepository.findByExamId(1L).size());
-//        assertThat(questionsRepository.findByExamId(1L).size()).isEqualTo(2);
-//    }
-//
-//    @Test
-//    void hi(){
-//
-//    }
-/*    @Test
-    void deleteQuestionsByExamId(){
-      //  assertThat(questionsRepository.findByExamId(1L).size()).isEqualTo(2);
-        boolean deleted = questionsService.deleteQuestionsByExamId(1L);
-        assertThat(deleted).isEqualTo(true);
-        assertThat(questionsRepository.findByExamId(1L).size()).isEqualTo(0);
-    }*/
-
-//    @Test
-//    void searchFromQuestionsTest() {
-//        Map<String, List<String>> tagsMap = new HashMap<>();
-//        tagsMap.put("category", List.of("법"));
-//        //tagsMap.put("year", List.of("20년도"));
-//
-//        List<String> includes = List.of("Lab화이팅");
-//
-//        assertThat(questionsRepository.findByExamId(1L).size()==2);
-//
-//        QuestionsSearch questionsSearch = QuestionsSearch.builder()
-//                .tagsMap(new HashMap<String, List<String>>())
-//                .includes(new ArrayList<>())
-//                .build();
-//
-//        QuestionsList questionsList = questionsService.searchFromQuestions(1L, questionsSearch);
-//
-//
-//        for (Question question : questionsList) {
-//            //assertThat(question.getTagsMap().get("category").contains("법"));
-//            //assertThat(question.getTagsMap().get("year").contains("20년도"));
-//            for (String include : includes) {
-//                assertThat(question.getQuestion().contains(include));
-//            }
-//        }
-//    }
+    }
 }

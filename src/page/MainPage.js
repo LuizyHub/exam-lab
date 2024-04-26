eb2c7525af7802e254865708ec9c9af64593bc45
binnay <clala0910@naver.com>
2024-04-26 15:16:00 +0900

    style : 메인페이지 css 추가

--- src/page/MainPage.js
+++ src/page/MainPage.js
@@ -11,6 +11,7 @@ export default function MainPage(){
     const [userName, setUserName] = useState('');
     const [loginStatus, setLoginStatus] = useState(false);
 
+    
 
     // 클릭시 자동 로그인
     // 개발을 위한 목적, 추후 삭제
@@ -26,7 +27,20 @@ export default function MainPage(){
             console.error('Login failed', error);
         }
     };
+
+    // 로그아웃 버튼
+    const handleLogout = async () => {
+        try{
+            const response = await axios.post('/api/v1/users/logout');
+            console.log("Logout successful", response);
+            setLoginStatus(false);
+
+        } catch(error) {
+            console.error('Logout failed', error);
+        }
+    }
     
+    // 로그인 상태 받아오기
     const handleLoginState = async () => {
         try {
             const response = await axios.get('/api/v1/users/status');
@@ -38,6 +52,11 @@ export default function MainPage(){
             setLoginStatus(login);
         } catch(error){
             console.error('Login failed', error);
+            // 401 에러가 발생하면 로그인되지 않았음을 알리는 알림창을 띄운다.
+            if (error.response && error.response.status === 401) {
+                alert('로그인되지 않았습니다. 다시 로그인해주세요.');
+                
+            }
         }
     };
     
@@ -45,15 +64,23 @@ export default function MainPage(){
 
     return(
         <div className="main-page">
-            <NavigationBar userName={userName} login={loginStatus} />
-            <div className="page-content" style={{paddingLeft:"30px"}}>
-                <h3 style={{paddingTop:"50px"}}> 쉽고 빠르게 나만의 시험지 만들기</h3>
-                <img src="/img/examLab_logo.png" alt="logo" style={{width:"25%"}}/>
-                <br />
-                <img src="/img/mainImage.png" alt="Main Image" style={{width:"50%"}} />
+            <div className="page-content" style={{ position: 'relative' }}>
+                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', width: '100%', height: '400px', background: 'linear-gradient(102.06deg, #E0F9F8 12.5%, #E2E6FA 98.35%)', top: 0, left: 0 }}>
+                    <div style={{ paddingLeft: '30px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <h3 style={{ position: 'relative' }}>쉽고 빠르게 나만의 시험지 만들기</h3>
+                    <img src="/img/examLab_logo.png" alt="logo" style={{ width: '200px', position: 'relative' }} />
+                    </div>
+                    <div style={{ paddingRight: '30px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <img src="/img/mainImage.png" alt="Main Image" style={{ width: '500px', position: 'relative' }} />
+                    </div>
+                </div>
+            </div>
+
+
+            
                 <nav>
                     <button className='sample-navigate-button'>
-                        <Link to='/select' style={{ textDecoration: 'none', color: 'inherit' }}>기출문제</Link>
+                        <Link to='/exams' style={{ textDecoration: 'none', color: 'inherit' }}>시험지 제작하기</Link>
                     </button>
                     <button className='myExam-navigate-button'>
                         <Link to='/add' style={{ textDecoration: 'none', color: 'inherit' }}>나만의 시험지</Link>
@@ -61,12 +88,13 @@ export default function MainPage(){
                 </nav>
 
                 <button onClick={handleAutoLogin}>자동 로그인</button>  
+                <button onClick={handleLogout}>로그아웃</button>
                 <button onClick={handleLoginState}>로그인정보 받아오기</button>
-               
-            </div>
+                
             <footer>
                 <Bottom />
             </footer>
+            <NavigationBar userName={userName} login={loginStatus} />
         </div>
     );
 }

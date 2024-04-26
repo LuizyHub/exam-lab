14eb255fa71dfeafbe38a95f9e5a8ebc9fad4e5f
binnay <125789787+binnay@users.noreply.github.com>
2024-04-26 15:38:32 +0900

    [FE] feat: 로그인 정보를 받아올 수 있다. (#76)
    * refactor : mainPage 수정, css 변경, exam-lab.store API 주소로 변경
    
    * test : login test
    
    로그인 정보를 보내고 받아올 수 있다.
    
    * feature: proxy set #61
    
    - api요청을 위한 axios 인스턴스 생성
    - /src/setupProxy.js 파일 설정
    
    * feature: axios로 동일하게 사용 가능하도록 변경
    
    - /users/login /users/add 도 동일하게 사용 가능하도록 적용
    
    * feat : api에 맞게 수정
    
    - proxy를 사용한 API 주소로 수정
    
    * feat: 수능영어 문제 데이터 불러오기
    
    - json-server 사용
    - 표 기능 처리
    - 밑줄 기능 처리
    - 지문 박스 처리
    - h1, h2, h3 처리
    
    * style : 네비게이션바 스타일 추가
    
    - 메인페이지 이미지 추가
    - 네비게이션바 스타일 추가
    
    * style : 메인페이지 css 추가
    
    * refactor : 페이지 url 일부 수정
    
    ---------
    
    Co-authored-by: luizy <luizy991212@gmail.com>

--- src/page/MainPage.js
+++ src/page/MainPage.js
@@ -1,19 +1,100 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import NavigationBar from '../components/NavigationBar';
-import Navigate from '../components/Navigate';
 import Bottom from '../components/Bottom';
+import './MainPage.css'
+import axios from 'axios';
+
 
 export default function MainPage(){
+
+    const [userName, setUserName] = useState('');
+    const [loginStatus, setLoginStatus] = useState(false);
+
+    
+
+    // 클릭시 자동 로그인
+    // 개발을 위한 목적, 추후 삭제
+    const handleAutoLogin = async () => {
+        try {
+            const response = await axios.post('/api/v1/users/login', {  
+                "password": "lab111!",
+                "user_id": "lab1@gmail.com"
+            });
+            
+            console.log('Login successful', response);    
+        } catch(error) {
+            console.error('Login failed', error);
+        }
+    };
+
+    // 로그아웃 버튼
+    const handleLogout = async () => {
+        try{
+            const response = await axios.post('/api/v1/users/logout');
+            console.log("Logout successful", response);
+            setLoginStatus(false);
+
+        } catch(error) {
+            console.error('Logout failed', error);
+        }
+    }
+    
+    // 로그인 상태 받아오기
+    const handleLoginState = async () => {
+        try {
+            const response = await axios.get('/api/v1/users/status');
+            console.log('Login successful', response);
+            const { user_name, login } = response.data; 
+            console.log('User name:', user_name);
+            console.log('Login status:', login);
+            setUserName(user_name);
+            setLoginStatus(login);
+        } catch(error){
+            console.error('Login failed', error);
+            // 401 에러가 발생하면 로그인되지 않았음을 알리는 알림창을 띄운다.
+            if (error.response && error.response.status === 401) {
+                alert('로그인되지 않았습니다. 다시 로그인해주세요.');
+                
+            }
+        }
+    };
+    
+  
+
     return(
         <div className="main-page">
-            <NavigationBar />
-            <div className="page-content">
-                <h1> 시험지 연구소 </h1>
-                <Navigate />
-                <img src="https://previews.123rf.com/images/anawat/anawat1601/anawat160100284/50001062-%EC%97%B0%EA%B5%AC%EC%9B%90%EC%9D%80-%EC%8B%A4%ED%97%98%EC%8B%A4%EC%97%90%EC%84%9C-%ED%99%94%ED%95%99-%EB%B0%A9%EC%A0%95%EC%8B%9D-%EB%B0%B0%EA%B2%BD%EC%9C%BC%EB%A1%9C-%EC%8B%9C%ED%97%98%EA%B4%80%EC%97%90-%EC%8B%9C%EC%95%BD%EC%9D%84-%EB%96%A8%EC%96%B4.jpg" alt="Main Image" />
+            <div className="page-content" style={{ position: 'relative' }}>
+                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', width: '100%', height: '400px', background: 'linear-gradient(102.06deg, #E0F9F8 12.5%, #E2E6FA 98.35%)', top: 0, left: 0 }}>
+                    <div style={{ paddingLeft: '30px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <h3 style={{ position: 'relative' }}>쉽고 빠르게 나만의 시험지 만들기</h3>
+                    <img src="/img/examLab_logo.png" alt="logo" style={{ width: '200px', position: 'relative' }} />
+                    </div>
+                    <div style={{ paddingRight: '30px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <img src="/img/mainImage.png" alt="Main Image" style={{ width: '500px', position: 'relative' }} />
+                    </div>
+                </div>
             </div>
+
+
+            
+                <nav>
+                    <button className='sample-navigate-button'>
+                        <Link to='/exams' style={{ textDecoration: 'none', color: 'inherit' }}>시험지 제작하기</Link>
+                    </button>
+                    <button className='myExam-navigate-button'>
+                        <Link to='/add' style={{ textDecoration: 'none', color: 'inherit' }}>나만의 시험지</Link>
+                    </button>
+                </nav>
+
+                <button onClick={handleAutoLogin}>자동 로그인</button>  
+                <button onClick={handleLogout}>로그아웃</button>
+                <button onClick={handleLoginState}>로그인정보 받아오기</button>
+                
             <footer>
                 <Bottom />
             </footer>
+            <NavigationBar userName={userName} login={loginStatus} />
         </div>
     );
 }
--- src/page/SelectExamPage.js
+++ src/page/SelectExamPage.js
@@ -1,54 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import './SelectExam.css';
+import axios from "axios";
+import './SelectExamPage.css';
+
 
 export default function SelectExamPage() {
   const navigate = useNavigate();
-  const [selectedExamType, setSelectedExamType] = useState(""); //시험지 종류
-  const [selectedSubExamType, setSelectedSubExamType] = useState(""); // 세부 시험지 종류
+  const [exams, setExams] = useState([]);
   const [isDataLoaded, setIsDataLoaded] = useState(false); // 데이터 로딩 완료 플래그
 
   useEffect(() => {
-    if (selectedExamType && selectedSubExamType) {
-      if(isDataLoaded) {
-        console.log(selectedExamType);
-        console.log(selectedSubExamType);
-        navigate('/selectQuestion', { state: { selectedExamType, selectedSubExamType } }); // 선택된 값으로 다음 화면으로 이동하는 로직
-      }
-    }
-  }, [selectedSubExamType, selectedSubExamType, isDataLoaded]);
+    axios.get(`/api/v1/exams`)
+    .then (response => {
+      console.log(response.data);
+      setExams(response.data.exams); 
+      setIsDataLoaded(true); 
+    })
+    .catch(error => {
+      // 에러 처리
+      console.error(error);
+    });
+  }, []);
+
+  
 
   // 시험지 종류 선택
-  const handleExamTypeClick = (e) => {
-    setSelectedExamType(e.target.value);
+  const handleExamTypeClick = (examId, examTitle) => {
+    navigate('/selectQuestion', { state: { examId, examTitle } }); // exam_id와 title 전달
   }
   
-  const handleSubExamTypeClick = (e) => {
-    setSelectedSubExamType(parseInt(e.target.value));
-    setIsDataLoaded(true);
-  }
- 
   return (
     <div>
       <div className="examList">
-        <button value="운전면허" onClick={handleExamTypeClick} >운전면허</button>
-        <button value="수능">수능</button>
-        <button value="컴퓨터활용능력">컴퓨터활용능력</button>
+        {exams.map(exam => (
+          <button key={exam.exam_id} onClick={() => handleExamTypeClick(exam.exam_id, exam.exam_title)}>
+            {exam.exam_title}
+          </button>
+        ))}
       </div>
-   
-      {selectedExamType && (
-        <select onChange={handleSubExamTypeClick}>
-          <option value="">선택</option>
-          <option value={1}>1종 보통</option>
-          <option value={2}>2종 보통</option>
-          <option value={3}>대형</option>
-          <option value={4}>특수</option>
-          <option value={5}>소형</option>
-          <option value={6}>원동기</option>
-        </select>
-      )}
-
     </div>
   )
 }
-

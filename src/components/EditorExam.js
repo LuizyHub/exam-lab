506aecbd5be364d67014ec4fc62fd2e9baf7c13d
4Bee <4bee.code@gmail.com>
2024-05-01 16:25:37 +0900

    refactor: 변수명 재정의와 post,delete,put 별로 함수 분리

--- src/components/EditorExam.js
+++ src/components/EditorExam.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { handleToolClick, handleImgToolClick } from '../function/toolHandle';
 import { useImage } from '../function/useImage';
 import { dataHandle } from '../function/dataHandle';
@@ -9,11 +9,8 @@ import axios from 'axios';
 export default function EditorExam() {
   //from import
   const { isImageSize, handleImgSize, handleImageSelect } = useImage();
-  const { handleContent, handleFileObject, handleIdContent, imageReplace } = dataHandle();
-  // const { sendData } = formData();
-  // const [contentType1, setContentType1] = useState('type');
-  // const [contentType2, setContentType2] = useState('type');
-  // const [contentType3, setContentType3] = useState('type');
+  const { handleFileObject, handleIdContent, imageReplace } = dataHandle();
+  // const { sendPostData } = formData();
 
   const imageSelectorRef1 = useRef(null); // 파일 선택 input 요소에 접근에 대한 참조
   const imageSelectorRef2 = useRef(null); // 파일 선택 input 요소에 접근에 대한 참조
@@ -25,20 +22,18 @@ export default function EditorExam() {
   const editorRef3 = useRef(null);
 
   const [isUrlOut, setUrlOut] = useState([]);
+  const [isUrlOutId, setUrlOutId] = useState([]);
   const [isUrlIn, setUrlIn] = useState([]);
   const [isUrlInId, setUrlInId] = useState([]);
   //단일로 수정
   const [isData, setData] = useState({
     question: '',
     options: [],
-    imageUrlOut: [],
-    imageUrlIn: [],
   });
 
-  // let ID = "";
   const [ID, setID] = useState("");
 
-  const sendData = () => {
+  const sendPostData = () => {
 
     const URL = '/api/v1/exams/3/questions'
     const formData = new FormData();
@@ -49,123 +44,92 @@ export default function EditorExam() {
       questionImagesTextIn.push(questionImage);
     });
 
+    const questionImagesTextOut = [];
+    isUrlOut.forEach(image => {
+      const questionImage = { url: "", description: "설명", attribute: "속성" };
+      questionImagesTextOut.push(questionImage);
+    });
+
     const questionUploadInfo = new Blob([JSON.stringify({
       type: "객관식",
       question: isData.question,
       options: isData.options,
-      //loop로 이미지 개수 만큼 객체 생성
       questionImagesTextIn: questionImagesTextIn,
+      questionImagesTextOut: questionImagesTextOut,
       answers: ["0"],
       tags: { "category": ["test"] },
       commentary: ""
+      //해답지
       // commentaryImagesTextIn: [{ url: "", description: "설명", attribute: "속성" }],
       // commentaryImagesTextOut: [{ url: "", description: "설명", attribute: "속성" }]
     })], {
       type: 'application/json'
     });
     formData.append('questionUploadInfo', questionUploadInfo);
-    // formData.append('questionImagesIn', isUrlIn[0]);
+
     isUrlIn.forEach((image) => {
       console.log(image.name);
       formData.append('questionImagesIn', image);
     });
-    // formData.append(`type`, '객관식');
-    // formData.append('question', isData.question); // 질문
-    // isData.options.forEach((option, index) => {
-    //   formData.append(`options[${index}]`, option); // 선택지
-    // });
-    // formData.append(`questionImagesIn`, isUrlIn[0]);
-    // isUrlIn.forEach((url) => {
-    //   // const imageData = {
-    //   //   url: "",
-    //   //   description: "설명",
-    //   //   attribute: "속성"
-    //   // };
-    //   //   // FormData에 해당 객체를 추가합니다.
-    //   //   formData.append(`questionImagesIn`, JSON.stringify(imageData));
-    //   formData.append(`questionImagesIn`, url);
-    // });
-    // isUrlOut.forEach((url, index) => {
-    //   const imageData = {
-    //     url: url,
-    //     description: "",
-    //     attribute: ""
-    //   };
-    //   // FormData에 해당 객체를 추가합니다.
-    //   formData.append(`questionImagesIn[${index}]`, JSON.stringify(imageData));
-    // });
-    // formData.append(`answers[]`, '');
+    //questionImagesOut
+    isUrlOut.forEach((image) => {
+      console.log(image.name);
+      formData.append('questionImagesOut', image);
+    });
 
-    // // tags 객체에 빈 배열 추가
-    // formData.append(`tags[category][]`, '');
-
-    // // commentary에 빈 문자열 추가
-    // formData.append(`commentary`, '');
-
-    // // commentaryImagesTextIn 배열에 빈 객체 추가
-    // formData.append(`commentaryImagesTextIn[][url]`, '');
-    // formData.append(`commentaryImagesTextIn[][description]`, '');
-    // formData.append(`commentaryImagesTextIn[][attribute]`, '');
-
-    // // commentaryImagesTextOut 배열에 빈 객체 추가
-    // formData.append(`commentaryImagesTextOut[][url]`, '');
-    // formData.append(`commentaryImagesTextOut[][description]`, '');
-    // formData.append(`commentaryImagesTextOut[][attribute]`, '');
-    // try {
-    //   // axios를 사용하여 FormData를 서버로 전송
-    //   const response = await axios.post(URL, formData, {
-    //     headers: {
-    //     }
-    //   });
-
-    //   console.log(response.data); // 서버에서 받은 응답 데이터
-    //   alert("저장");
-    // } catch (error) {
-    //   // 요청이 실패했을 때 실행되는 코드
-    //   console.error('에러 발생:', error);
-    //   // 오류 처리 추가 가능
-    // }
-
-
-    // const file = new File([isUrlIn[0]], 'image.png', { type: 'image/png' });
-    // formData.append('questionImagesIn', file);
     console.log([...formData.entries()]);
 
     axios.post(URL, formData)
       .then((response) => {
-        // 응답 받은 메시지 상태 업데이트
         console.log(response.data.message);
         console.log(response.data);
-        //id
         const message = response.data.message;
         setID(message);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const sendDeleteData = () => {
+    console.log("삭제");
+    console.log(isUrlIn);
+    console.log(ID);
+    const URL = `/api/v1/questions/${ID}`
+    // const URL = `/api/v1/questions/671eb7d7-2cc6-446d-8790-83cb0bd8cddc`
+    axios.delete(URL, {
+    })
+      .then((response) => {
+        console.log(response.data.message);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  const sendPutData = () => {
+    console.log("수정");
+    const URL = `/api/v1/questions`
+    console.log(ID)
+    const requestData = {
+      id: ID,
+      question: isData.question,
+      options: isData.options,
+      answers: ["0"],
+      commentary: "",
+      tags: { "category": ["test"] },
+    };
+
+    axios.put(URL, requestData)
+      .then((response) => {
+        console.log(response.data.message);
+        console.log(response.data);
       })
       .catch((error) => {
         // 오류 처리
         console.log(error);
       });
-  };
-
-  // useEffect(() => {
-  //   if (ID) {
-  //     sendData();
-  //   }
-  // }, [ID]);
-
-  // const handleContentType1 = (e) => {
-  //   const contentType = e.currentTarget.value;
-  //   setContentType1(contentType);
-  // }
-
-  // const handleContentType2 = (e) => {
-  //   const contentType = e.currentTarget.value;
-  //   setContentType2(contentType);
-  // }
-
-  // const handleContentType3 = (e) => {
-  //   const contentType = e.currentTarget.value;
-  //   setContentType3(contentType);
-  // }
+  }
 
   return (
     <div>
@@ -194,14 +158,12 @@ export default function EditorExam() {
         style={{ display: 'none' }}
         ref={imageSelectorRef1}
         onChange={(e) => {
-
           //blob : 로컬 이미지 가져온 url값을 저장하고 해당 이미지를 생성해서 렌더링하기 수행한다
           const result = handleImageSelect(e, editorRef1);
           console.log(result);
           setUrlIn(prevState => [...prevState, result]);
           setUrlInId(prevState => [...prevState, result.name])
           //업로드 되면서 공백 없이 바로 question에 존재하는 html입력 값을 확인
-          //-> 초기값ㄴ
           const isQuestion = editorRef1.current.innerHTML;//
           const imageReplaceResult = imageReplace(isQuestion);
           console.log(imageReplaceResult);
@@ -210,23 +172,9 @@ export default function EditorExam() {
               ...prevState,
               question: imageReplaceResult
             }));
-
-          // 새로운 이미지 업로드가 되면 객체 생성
-          // const newImageObject = {
-          //   url: '',
-          //   description: '', // 이미지 설명
-          //   attribute: '' // 이미지 속성
-          // };
-
-          // setData(prevState => ({
-          //   ...prevState,
-          //   imageUrlIn: [...prevState.imageUrlIn, newImageObject]
-          // }));
-
         }}
       />
 
-      {/* contentEditable */}
       <div
         className="editor"
         contentEditable="true"
@@ -263,12 +211,8 @@ export default function EditorExam() {
         }} // 이미지 붙여넣기 동작 막기
 
 
-        onInput={() => { //이곳에서 type이 변경되면 변경된다는 것이 나와야함
-          //==================================== setData
+        onInput={() => {
           const isQuestion = editorRef1.current.innerHTML;
-
-          //해당 setData는 이미지가 업로드
-          //isQuestion 부분이 한번 가공되고 저장이 되어야 함
           const imageReplaceResult = imageReplace(isQuestion);
           console.log(imageReplaceResult);
           setData(
@@ -276,35 +220,17 @@ export default function EditorExam() {
               ...prevState,
               question: imageReplaceResult
             }));
-
-          //==================================== setUrlIn
           const resultEdit = handleFileObject(editorRef1, isUrlInId, isUrlIn);
           const resultId = handleIdContent(editorRef1, isUrlInId)
           console.log(resultEdit);
           setUrlIn(resultEdit);
           setUrlInId(resultId);
-
-          // const imageUrlInLength = resultEdit.length;
-
-          // // imageUrlIn의 길이만큼 객체가 존재하지 않을 경우 새로운 객체를 추가
-          // setData(prevState => ({
-          //   ...prevState,
-          //   imageUrlIn: imageUrlInLength > 0 ? prevState.imageUrlIn : Array.from({ length: imageUrlInLength }, () => ({}))
-          // }));
-
         }}
 
         style={{ padding: '16px 24px', border: '1px solid #D6D6D6', borderRadius: '4px', width: '600px' }}
       />
 
       {/* ------------------------------------------------------------------------ */}
-
-      {/* <select value={contentType2} onChange={handleContentType2}>
-        <option value="type">Select</option>
-        <option value="문제">문제</option>
-        <option value="이미지">이미지</option>
-        <option value="선택지">선택지</option>
-      </select> */}
 
       <EditorTool
         editorRef={editorRef2}
@@ -326,17 +252,8 @@ export default function EditorExam() {
 
           const result = handleImageSelect(e, editorRef2);
           setUrlOut(prevState => [...prevState, result]);
-
-          // const newImageObject = {
-          //   url: '',
-          //   description: '', // 이미지 설명
-          //   attribute: '' // 이미지 속성
-          // };
-
-          // setData(prevState => ({
-          //   ...prevState,
-          //   imageUrlOut: [...prevState.imageUrlOut, newImageObject]
-          // }));
+          //blob : 로컬 이미지 가져온 url값을 저장하고 해당 이미지를 생성해서 렌더링하기 수행한다
+          setUrlOutId(prevState => [...prevState, result.name])
         }}
       />
       <div
@@ -375,33 +292,17 @@ export default function EditorExam() {
         }} // 이미지 붙여넣기 동작 막기
 
         onInput={() => {
-          const result = handleContent(editorRef2, isUrlOut);
-          setUrlOut(result);
-          console.log(result);
-
-          const resultEdit = handleContent(editorRef2, isUrlOut);
-          setUrlIn(resultEdit);
-
-          const imageUrlOutLength = resultEdit.length;
-
-          // imageUrlIn의 길이만큼 객체가 존재하지 않을 경우 새로운 객체를 추가
-          setData(prevState => ({
-            ...prevState,
-            imageUrlOut: imageUrlOutLength > 0 ? prevState.imageUrlOut : Array.from({ length: imageUrlOutLength }, () => ({}))
-          }));
+          const resultEdit = handleFileObject(editorRef2, isUrlOutId, isUrlOut);
+          const resultId = handleIdContent(editorRef2, isUrlOutId)
+          console.log(resultEdit);
+          setUrlOut(resultEdit);
+          setUrlOutId(resultId);
         }}
 
         style={{ padding: '16px 24px', border: '1px solid #D6D6D6', borderRadius: '4px', width: '600px' }}
       />
 
       {/* ------------------------------------------------------------------------ */}
-
-      {/* <select value={contentType3} onChange={handleContentType3}>
-        <option value="type">Select</option>
-        <option value="문제">문제</option>
-        <option value="이미지">이미지</option>
-        <option value="선택지">선택지</option>
-      </select> */}
 
       <EditorTool
         editorRef={editorRef3}
@@ -481,125 +382,14 @@ export default function EditorExam() {
       />
 
       <button type='submit' onClick={() => {
-        sendData();
+        sendPostData();
       }}>생성</button>
       <button onClick={() => {
-        console.log("삭제");
-        console.log(isUrlIn);
-        console.log(ID);
-        const URL = `/api/v1/questions/${ID}`
-        // const URL = `/api/v1/questions/671eb7d7-2cc6-446d-8790-83cb0bd8cddc`
-        axios.delete(URL, {
-        })
-          .then((response) => {
-            console.log(response.data.message);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
+        sendDeleteData();
       }
       }>삭제</button>
-
       <button onClick={() => {
-        console.log("수정");
-        const URL = `/api/v1/questions`
-        // const formData = new FormData();
-        console.log(ID)
-        // const questionImagesTextIn = [];
-        // isUrlIn.forEach(image => {
-        //   const questionImage = { url: "", description: "설명", attribute: "속성" };
-        //   questionImagesTextIn.push(questionImage);
-        // });
-
-        const requestData = {
-          id: ID,
-          question: isData.question,
-          options: isData.options,
-          answers: ["0"],
-          commentary: "",
-          tags: { "category": ["test"] },
-        };
-        // formData.append('questionUploadInfo', questionUploadInfo);
-        // // formData.append('questionImagesIn', isUrlIn[0]);
-        // isUrlIn.forEach((image) => {
-        //   console.log(image.name);
-        //   formData.append('questionImagesIn', image);
-        // });
-        // formData.append(`type`, '객관식');
-        // formData.append('question', isData.question); // 질문
-        // isData.options.forEach((option, index) => {
-        //   formData.append(`options[${index}]`, option); // 선택지
-        // });
-        // formData.append(`questionImagesIn`, isUrlIn[0]);
-        // isUrlIn.forEach((url) => {
-        //   // const imageData = {
-        //   //   url: "",
-        //   //   description: "설명",
-        //   //   attribute: "속성"
-        //   // };
-        //   //   // FormData에 해당 객체를 추가합니다.
-        //   //   formData.append(`questionImagesIn`, JSON.stringify(imageData));
-        //   formData.append(`questionImagesIn`, url);
-        // });
-        // isUrlOut.forEach((url, index) => {
-        //   const imageData = {
-        //     url: url,
-        //     description: "",
-        //     attribute: ""
-        //   };
-        //   // FormData에 해당 객체를 추가합니다.
-        //   formData.append(`questionImagesIn[${index}]`, JSON.stringify(imageData));
-        // });
-        // formData.append(`answers[]`, '');
-
-        // // tags 객체에 빈 배열 추가
-        // formData.append(`tags[category][]`, '');
-
-        // // commentary에 빈 문자열 추가
-        // formData.append(`commentary`, '');
-
-        // // commentaryImagesTextIn 배열에 빈 객체 추가
-        // formData.append(`commentaryImagesTextIn[][url]`, '');
-        // formData.append(`commentaryImagesTextIn[][description]`, '');
-        // formData.append(`commentaryImagesTextIn[][attribute]`, '');
-
-        // // commentaryImagesTextOut 배열에 빈 객체 추가
-        // formData.append(`commentaryImagesTextOut[][url]`, '');
-        // formData.append(`commentaryImagesTextOut[][description]`, '');
-        // formData.append(`commentaryImagesTextOut[][attribute]`, '');
-        // try {
-        //   // axios를 사용하여 FormData를 서버로 전송
-        //   const response = await axios.post(URL, formData, {
-        //     headers: {
-        //     }
-        //   });
-
-        //   console.log(response.data); // 서버에서 받은 응답 데이터
-        //   alert("저장");
-        // } catch (error) {
-        //   // 요청이 실패했을 때 실행되는 코드
-        //   console.error('에러 발생:', error);
-        //   // 오류 처리 추가 가능
-        // }
-
-
-        // const file = new File([isUrlIn[0]], 'image.png', { type: 'image/png' });
-        // formData.append('questionImagesIn', file);
-        // console.log([...formData.entries()]);
-
-        axios.put(URL, requestData)
-          .then((response) => {
-            // 응답 받은 메시지 상태 업데이트
-            console.log(response.data.message);
-            console.log(response.data);
-            //id
-            // const message = response.data.message;
-            // ID = message;
-          })
-          .catch((error) => {
-            // 오류 처리
-            console.log(error);
-          });
+        sendPutData();
       }}>수정</button>
 
       <button onClick={() => {
@@ -613,8 +403,6 @@ export default function EditorExam() {
           + "\n저장된 API options 값 : " + isData.options
           + "\n저장된 API isUrlIn 값 : " + isData.isUrlIn
         );
-        console.log("저장된 imageUrlIn 객체 값 : ", isData.imageUrlIn);
-        console.log("저장된 imageUrlOut 객체 값 : ", isData.imageUrlOut);
         isUrlInId.forEach((imageId) => { console.log(imageId) })
         console.log(ID);
       }}>확인</button>

5ce0dbd21a88c82de9b9a4d0942a2b4aa24f7fa6
kimjisu <comjisu0311@naver.com>
2024-05-02 11:17:39 +0900

    refeat: Question update 데이터 전환 로직 dto로 이전 #78

--- src/main/java/capstone/examlab/questions/dto/update/QuestionUpdateDto.java
+++ src/main/java/capstone/examlab/questions/dto/update/QuestionUpdateDto.java
@@ -1,8 +1,10 @@
 package capstone.examlab.questions.dto.update;
 
+import capstone.examlab.questions.entity.QuestionEntity;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -15,4 +17,32 @@ public class QuestionUpdateDto {
     private List<Integer> answers;
     private String commentary;
     private Map<String, List<String>> tags;
+
+    //AI문제 QuestionUpadteDto -> Question
+    public QuestionEntity toDocument(Long examId, String uuid, String type) {
+        return QuestionEntity.builder()
+                .id(uuid)
+                .examId(examId)
+                .type(type)
+                .question(this.question)
+                .questionImagesIn(new ArrayList<>())
+                .questionImagesOut(new ArrayList<>())
+                .options(this.options)
+                .answers(this.answers)
+                .commentary(this.commentary)
+                .commentaryImagesIn(new ArrayList<>())
+                .commentaryImagesOut(new ArrayList<>())
+                .tagsMap(this.tags)
+                .build();
+    }
+
+    //Question내용 덮어쓰기
+    public QuestionEntity updateDocument(QuestionEntity question) {
+        question.setQuestion(this.question);
+        question.setOptions(this.options);
+        question.setAnswers(this.answers);
+        question.setCommentary(this.commentary);
+        question.setTagsMap(this.tags);
+        return question;
+    }
 }

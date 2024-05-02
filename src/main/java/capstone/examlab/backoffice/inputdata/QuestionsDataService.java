7d85ef9cb841f25773f7551f781c4b807063cbaa
kimjisu <comjisu0311@naver.com>
2024-05-02 14:30:13 +0900

    refeat: Question관련 폴더명, 네이밍 수정 #78

--- src/main/java/capstone/examlab/backoffice/inputdata/QuestionsDataService.java
+++ src/main/java/capstone/examlab/backoffice/inputdata/QuestionsDataService.java
@@ -1,9 +1,9 @@
 package capstone.examlab.backoffice.inputdata;
 
-import capstone.examlab.questions.entity.QuestionEntity;
+import capstone.examlab.questions.documnet.Question;
 
 import java.util.List;
 
 public interface QuestionsDataService {
-    void saveQuestions(List<QuestionEntity> questionEntities);
+    void saveQuestions(List<Question> questionEntities);
 }

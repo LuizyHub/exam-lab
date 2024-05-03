aa2ecef6382647f26a4e99b31107c4bf9f4c43ff
4Bee <4bee.code@gmail.com>
2024-05-03 23:27:14 +0900

    feat: 문제 템플릿을 삭제 할 수 있는 코드 구현

--- src/page/EditExam.js
+++ src/page/EditExam.js
@@ -6,9 +6,17 @@ import { useState } from "react";
 
 export default function EditExam() {
   const [isCreate, setCreate] = useState([]);
+
   const handleCreate = () => {
     // 새로운 컴포넌트를 생성하여 상태에 추가
     setCreate([...isCreate, <EditorExam key={isCreate.length} />]);
+  };
+
+  const handleDelete = (elementIndex) => {
+    // 인덱스를 사용하여 삭제할 컴포넌트를 제외한 배열을 만듭니다.
+    const deleteElement = isCreate.filter((_, index) => index !== elementIndex);
+    // 변경된 배열을 상태에 적용합니다.
+    setCreate(deleteElement);
   };
 
   return (
@@ -16,16 +24,18 @@ export default function EditExam() {
       <h1>Test</h1>
       <AttributeManager></AttributeManager>
       {/* AttributeManager 저장하기가 되어야 EditorExam이 활성화 */}
-
-      <div style={{ marginTop: '40px', padding: '16px 24px', border: '1px solid #D6D6D6', borderRadius: '4px', width: '700px' }}>
-        {isCreate.map((component, index) => (
-          <div key={index}>{component}</div>
-        ))}
-      </div>
-
       <button
         onClick={handleCreate}
       >생성</button>
+
+      {isCreate.map((component, index) => (
+        <div style={{ marginTop: '40px', padding: '16px 24px', border: '1px solid #D6D6D6', borderRadius: '4px', width: '700px' }}>
+          <button onClick={() => handleDelete(index)}>삭제</button>
+          <div key={index}>{component}</div>
+        </div>
+      ))}
+
+
 
 
       {/* <EditorExam></EditorExam>

d1fd2e5dce1970a92496efba124a35c67c06ecda
4Bee <4bee.code@gmail.com>
2024-05-03 23:18:45 +0900

    feat : 문제 템플릿을 생성 할 수 있는 코드 구현

--- src/page/EditExam.js
+++ src/page/EditExam.js
@@ -1,14 +1,33 @@
 // import { useRef } from "react";
 import EditorExam from "../components/EditorExam"
 import AttributeManager from "../components/AttributeManager"
+import { useState } from "react";
 // import { useDataHandle } from "../..//dataHandle";
+
 export default function EditExam() {
+  const [isCreate, setCreate] = useState([]);
+  const handleCreate = () => {
+    // 새로운 컴포넌트를 생성하여 상태에 추가
+    setCreate([...isCreate, <EditorExam key={isCreate.length} />]);
+  };
+
   return (
     <>
       <h1>Test</h1>
       <AttributeManager></AttributeManager>
       {/* AttributeManager 저장하기가 되어야 EditorExam이 활성화 */}
-      <EditorExam></EditorExam>
+
+      <div style={{ marginTop: '40px', padding: '16px 24px', border: '1px solid #D6D6D6', borderRadius: '4px', width: '700px' }}>
+        {isCreate.map((component, index) => (
+          <div key={index}>{component}</div>
+        ))}
+      </div>
+
+      <button
+        onClick={handleCreate}
+      >생성</button>
+
+
       {/* <EditorExam></EditorExam>
       <EditorExam></EditorExam> */}
     </>

b24a59b2cd4e556d549a59907f8de5254f97ce30
binnay <clala0910@naver.com>
2024-05-03 23:03:41 +0900

    feat : 시험에 대한 CRUD가 가능하다

--- src/page/SelectExamCreatePage.js
+++ src/page/SelectExamCreatePage.js
@@ -1,9 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
+import NavigationBar from '../components/NavigationBar';
 
-export default function SelectExamCreatePage(){
 
-    return (
-        <div>
-            
+export default function SelectExamCreatePage() {
+  const navigate = useNavigate();
+  const [exams, setExams] = useState([]);
+
+
+  useEffect(() => {
+    axios.get(`/api/v1/exams`)
+    .then (response => {
+      console.log(response.data);
+      setExams(response.data.exams); 
+
+    })
+    .catch(error => {
+      // 에러 처리
+      console.error(error);
+    });
+  }, []);
+
+  
+
+  // 시험 종류 선택
+  const handleExamTypeClick = (examId, examTitle) => {
+    navigate('/selectQuestion', { state: { examId, examTitle } }); // exam_id와 title 전달
+  }
+
+
+
+  
+  return (
+    <div>
+        <div className="exam-create-content">
+        <h1>나만의 시험지 제작하기</h1>
+        <h3>step1. 시험 선택하기</h3>
+        <div className="examList">
+          {exams.map(exam => (
+            <button key={exam.exam_id} onClick={() => handleExamTypeClick(exam.exam_id, exam.exam_title)}>
+              {exam.exam_title}
+            </button>
+          ))}
+          <button onClick={()=> {navigate('/editR')}}> + 시험 추가 
+          </button>
         </div>
-    );
+      </div>
+      <NavigationBar />
+
+    </div>
+  )
 }
+
--- src/page/SelectExamPage.js
+++ src/page/SelectExamPage.js
@@ -4,47 +4,99 @@ import axios from "axios";
 import './SelectExamPage.css';
 import NavigationBar from '../components/NavigationBar';
 
-
 export default function SelectExamPage() {
   const navigate = useNavigate();
   const [exams, setExams] = useState([]);
-  const [isDataLoaded, setIsDataLoaded] = useState(false); // 데이터 로딩 완료 플래그
+  // const [inputExamTitleModal, setInputExamTitleModal] = useState(false);
+  // const [newExamTitle, setNewExamTitle] = useState('');
 
   useEffect(() => {
     axios.get(`/api/v1/exams`)
-    .then (response => {
-      console.log(response.data);
-      setExams(response.data.exams); 
-      setIsDataLoaded(true); 
-    })
-    .catch(error => {
-      // 에러 처리
-      console.error(error);
-    });
+      .then(response => {
+        console.log(response.data);
+        setExams(response.data.exams);
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }, []);
 
-  
-
-  // 시험지 종류 선택
+  // 시험 종류 선택
   const handleExamTypeClick = (examId, examTitle) => {
-    navigate('/selectQuestion', { state: { examId, examTitle } }); // exam_id와 title 전달
+    navigate('/editR', { state: { examId, examTitle } });
   }
   
+
+  // 시험 삭제하기
+  const handleExamDelete = (examId) => {
+    axios.delete(`/api/v1/exams/${examId}`)
+      .then(response => {
+        console.log('success', response.data);
+        setExams(exams.filter(exam => exam.exam_id !== examId));
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  // // 시험 추가 모달 열기
+  // const openExamTitleModal = () => {
+  //   setInputExamTitleModal(true);
+  // }
+
+  // // 시험 추가 모달에서 시험 제목 입력
+  // const handleNewExamTitleChange = (event) => {
+  //   setNewExamTitle(event.target.value);
+  // }
+
+  // // 시험 추가 모달 닫기
+  // const closeExamTitleModal = () => {
+  //   setInputExamTitleModal(false);
+  //   setNewExamTitle(''); // 모달이 닫힐 때 입력값 초기화
+  // }
+
+  // // 새로운 시험 추가
+  // const addNewExam = () => {
+  //   // 여기에 새로운 시험 추가하는 로직 구현
+  //   console.log('새로운 시험 추가:', newExamTitle);
+  //   closeExamTitleModal(); // 모달 닫기
+
+  //   navigate('/editR');
+
+  // }
+
   return (
     <div>
-        <div className="exam-create-content">
-        <h1>나만의 시험지 제작하기</h1>
-        <h3>step1. 시험 선택하기</h3>
+      <div className="exam-create-content">
+        <h1>내 문제 관리하기</h1>
         <div className="examList">
           {exams.map(exam => (
-            <button key={exam.exam_id} onClick={() => handleExamTypeClick(exam.exam_id, exam.exam_title)}>
-              {exam.exam_title}
-            </button>
+            <div key={exam.exam_id}>
+              <button className="exam" onClick={() => handleExamTypeClick(exam.exam_id, exam.exam_title)}>
+                {exam.exam_title}
+              </button>
+              {exam.exam_id > 2 &&
+                <button onClick={() => handleExamDelete(exam.exam_id)}>시험 삭제하기</button>
+              }
+            </div>
           ))}
+          {/* <button onClick={openExamTitleModal}> + 시험 추가 </button> */}
+
         </div>
       </div>
       <NavigationBar />
+      <button onClick={()=> {navigate('/editR')}}> + 시험 추가 </button>
+      {/* {inputExamTitleModal &&
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeExamTitleModal}>&times;</span>
+            <h2>새로운 시험 추가</h2>
+            <input type="text" value={newExamTitle} onChange={handleNewExamTitleChange} />
+            <br /> <br />
+            <button onClick={addNewExam}>시험지 생성하러 가기</button>
+          </div>
+        </div>
+      } */}
     </div>
   )
 }
-

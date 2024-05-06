9f2ea831117c01addcc3770217b18fef5c01d46c
binnay <125789787+binnay@users.noreply.github.com>
2024-05-07 00:37:47 +0900

    [FE] EditPage로 컴포넌트 이동 (#100)
    * refactor: divide to function components
    
    * test: confirm POST delivery from through jsonserver
    
    * test: confirm POST from through jsonserver
    
    * test: confirm select type and POST data through jasonserver
    
    * refactor: fail to components editor
    
    * refactor: handleContentType function
    
    * refactor: release component about dataHandle and edtior
    
    * feat: fail to POST data to the jsonserver from the sessionStorage
    
    * feat: dataHandle clean code compelete
    
    * feat: image url extraction from blob
    
    * feat: 테스트를 위한 임시 커밋
    
    * feat: 각 편집 기능 및 편집 구현 완료 axios로 데이터 전송 구현
    
    * feat: cors문제 해결, 데이터 전송 확인 필요
    
    * feat: 이미지 테스트를 위한 임시 커밋
    
    * feat: 페이지 테스트를 위한 임시 커밋
    
    * feat: 페이지에 따른 접근 권한 설정
    
    * feat : 페이지 접근권한을 설정할 수 있다.
    
    - '내 문제 관리하기'와 '내 시험지 관리하기' 페이지는 로그인된 사용자만 접근 할 수 있다.
    - '나만의 시험지 제작하기' 페이지는 모두 접근 가능하다.
    - 사이드바 버튼을 클릭하면, 사이드바가 나온다.
    
    * refactor : 선택된 문제를 체크박스 형태로 보이도록 변경
    
    * feat : 시험에 대한 CRUD가 가능하다
    
    * refactor : 로그인과 사이드바 상태를 전역변수로 관리할 수 있다.
    
    * refactor : 사이드바에 페이지가 가려지지 않도록 이동할 수 있다.
    
    * feat : AI API와 연동할 수 있다.
    
    - [ ] pdf, txt, md 파일을 업로드할 수 있다.
    - [ ] 업로드되었던 파일을 불러올 수 있다.
    - [ ] 파일을 삭제할 수 있다.
    - [ ] AI가 만든 문제를 불러올 수 있다.
    
    * refactor : /exam/{examId} -> /exam/edit URL 수정
    
    * refactor : 경로명 수정
    
    * fix : EditExam 페이지로 컴포넌트를 이동
    
    개발 과정에서 서로 다른 파일에서 작업하여 혼동이 있어, EditExam 페이지로 컴포넌트를 이동시켰다.
    
    ---------
    
    Co-authored-by: 4Bee <4bee.code@gmail.com>

--- src/test/EditPage/EditExamRefactory.js
+++ src/test/EditPage/EditExamRefactory.js
@@ -1,42 +1,14 @@
-import { useRef, useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
 import EditorExam from "../../components/EditorExam";
-import AttributeManager from "../../components/AttributeManager";
 import AICreate from "../AI/AICreate";
 // import { useDataHandle } from "../..//dataHandle";
 
 export default function EditExam() {
 
-  const location = useLocation();
-
-  // 선택된 시험의 examId와 examTitle을 받아, AttributeManager 컴포넌트에게 props로 전달
-  const [examTitle, setExamTitle] = useState('');
-  const [examId, setExamId] = useState('');
-
-  console.log("Location:", location);
-  console.log("Exam ID:", examId);
-  console.log("Exam Title:", examTitle);
-
-  // 컴포넌트가 로드될 때 실행되는 훅
-  useEffect(() => {
-    // location.state가 정의되어 있을 때만 examId와 examTitle을 설정합니다.
-    if (location && location.state) {
-      const { examId, examTitle } = location.state;
-      setExamId(examId);
-      setExamTitle(examTitle);
-      console.log("Location:", location);
-      console.log("Exam ID:", examId);
-      console.log("Exam Title:", examTitle);
-
-    }
-  }, [location]);
 
   return (
     <>
-      <AttributeManager examId={examId} />
       <h1>Test</h1>
       <EditorExam></EditorExam>
-      <AICreate examId={examId}/>
       {/* <EditorExam></EditorExam>
       <EditorExam></EditorExam> */}
     </>

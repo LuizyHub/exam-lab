6eb9094467cfdbe0a7317fc6100f1c3a3be12ac3
luizy <luizy991212@gmail.com>
2024-05-10 12:49:09 +0900

    feature: 싱글토큰 검증 #118

--- src/main/java/capstone/examlab/util/Util.java
+++ src/main/java/capstone/examlab/util/Util.java
@@ -5,7 +5,7 @@ public final class Util {
     private Util() {}
 
     public static boolean isSingleToken(String str) {
-        if (!str.matches("^[a-zA-z가-힣0-9]+$")) throw new IllegalArgumentException("Invalid token");
-        return str.trim().split("\\s+").length == 1;
+        if (!str.matches("^[a-zA-z가-힣0-9]+$")) return false;
+        return true;
     }
 }

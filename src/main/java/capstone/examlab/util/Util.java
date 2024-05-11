ba6565ab76eeeb99ec2fda67cac52b7111711715
kimjisu <comjisu0311@naver.com>
2024-05-12 00:39:56 +0900

    feat: Question 조회 dto 검증 적용 #113

--- src/main/java/capstone/examlab/util/Util.java
+++ src/main/java/capstone/examlab/util/Util.java
@@ -5,7 +5,9 @@ public final class Util {
     private Util() {}
 
     public static boolean isSingleToken(String str) {
-        if (!str.matches("^[a-zA-z가-힣0-9]+$")) return false;
-        return true;
+        return str.matches("^[a-zA-Z가-힣0-9]+$");
+    }
+    public static boolean isSingleTokenWithUnderscore(String str) {
+        return str.matches("^[a-zA-Z가-힣0-9_]+$");
     }
 }

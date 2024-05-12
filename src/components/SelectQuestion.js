a260c4bc02862a7d563b068adc1342a4e3c887ab
binnay <125789787+binnay@users.noreply.github.com>
2024-05-12 20:05:00 +0900

    [FE] style: 전반적인 페이지에 스타일 적용 (#124)
    * refactor : EditExam페이지 내 사이드바 추가
    
    * refactor : 시험 속성 CRUD 보완
    
     - 속성과 시험 삭제시, 한번 더 삭제 메세지를 띠울 수 있다.
     - 사용자가 이해하기 용이하도록 속성과 태그에 default로 난이도-상,중,하를 준다.
     - 사용자가 이해하기 용이하도록 설명글을 제공한다.
    
    * fix : 옵션들의 번호를 출력할 수 있다.
    
    * refactor : 문제 검색 페이지 수정
    
    * style : 전반적인 페이지 스타일 적용
    
    - styled-components 사용
    
    * refactor: 파일 여부에 따른 버튼 설정
    
    - AI 문제 생성에서 파일이 없으면 업로드 버튼만 뜨고, 파일이 있으면 파일 삭제 버튼과 문제 생성 버튼이 뜬다.
    
    * fix : style-components 에러 해결
    
    * fix : styled-components 오류 수정
    
    * factor : 영어 문제 데이터 처리
    
    - box, content
    - h1, h2, h3
    - 표 처리
    
    * fix : styled-components 오류 수정

--- src/components/SelectQuestion.js
+++ src/components/SelectQuestion.js
@@ -2,7 +2,162 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router";
 import axios from 'axios';
 import ShowQuestionList from "./ShowQuestionList";
-import "../css/SelectQuestion.css";
+import styled from 'styled-components';
+
+
+const Container = styled.div`
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 20px;
+    margin-bottom: 20px;
+    margin-top: 30px;
+`;
+
+const Title = styled.h1`
+    text-align: center;
+`;
+
+const TagsContainer = styled.div`
+    margin-bottom: 10px;
+    display: flex;
+    flex-wrap: wrap;
+
+`;
+
+const TagButton = styled.button`
+    margin: 5px 10px; 
+    background-color: ${({ selected }) => selected ? '#EDFAFA' : '#FFFFFF'};
+    color: #2D2C2B;
+    border: 0.5px solid ${({ selected }) => selected ? '#C6E7E7' : '#E2E8EE'};
+    border-radius: 4px;
+    padding: 5px 8px;
+    cursor: pointer;
+    flex: 1; 
+    width: 220px;
+    height: 45px;
+    font-size: 14px;
+    &:hover {
+        background-color: #C6E7E7;
+    }
+`;
+    
+
+const KeywordList = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const KeywordContainer = styled.div`
+    margin-bottom: 5px;
+    display: flex;
+    align-items: center;
+`;
+
+const KeywordInput = styled.input`
+    width: 85%;
+    height: 30px;
+    margin-left: 10px;
+    margin-right: 10px;
+    padding: 5px 15px;
+    border : 1px solid #E2E8EE;
+    border-radius: 4px;
+    outline: none;
+    transition: border-color 0.2s; 
+    flex: 1;
+    &:focus {
+        border-color: #5BB6B4; 
+    }
+`;
+
+
+const Keyword = styled.button`
+    height: 25px;
+    padding: 0 10px;;
+    font-size: 12px;
+    border: 1px solid #C6E7E7;
+    background-color: #EDFAFA;
+    border-radius: 20px;
+    margin-top: 10px;
+    margin-left: 10px;
+    transition: background-color 0.3s, transform 0.2s;
+`;
+
+const DeleteKeywordImg = styled.img`
+    width: 8px;
+    margin-left: 8px;
+`;
+
+const SearchImg = styled.img`
+    width: 18px;
+    position: relative;
+    padding: 6px;
+    top: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+`;
+
+const QuestionCount = styled.div`
+    margin-bottom: 10px;
+`;
+
+const CountButton = styled.button`
+  margin: 5px 10px;
+  background-color: ${({ selected }) => (selected ? '#EDFAFA' : '#FFFFFF')};
+  color: #2D2C2B;
+  border: 0.5px solid ${({ selected }) => (selected ? '#C6E7E7' : '#E2E8EE')};
+  border-radius: 4px;
+  padding: 5px 8px;
+  cursor: pointer;
+  flex: 1;
+  width: 150px;
+  height: 45px;
+  font-size: 14px;
+
+  &:hover {
+    background-color: ${({ selected }) => (selected ? '#C6E7E7' : '#E2E8EE')};
+  }
+`;
+
+const CountInput = styled.input`
+  margin-left: 10px;
+  width: 150px;
+  height: 35px;
+  background-color: ${({ selected }) => (selected ? '#E8F6F6' : '#FFFFFF')};
+  color: #2D2C2B;
+  border: 0.5px solid ${({ selected }) => (selected ? '#E2E8EE' : '#E2E8EE')};
+  border-radius: 5px;
+  padding: 5px 10px;
+  cursor: pointer;
+  margin-right: 5px;
+  width: 130px;
+  text-align: center;
+
+  &:focus {
+    background-color: #E2E8EE;
+  }
+`;
+
+
+
+const SubmitButtonContainer = styled.div`
+    display: flex;
+    justify-content: center; 
+`;
+
+const SubmitButton = styled.input`
+    background-color: #5BB6B4;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+    margin-top: 10px;
+    align-self: center;
+
+    &:hover {
+        background-color: #C6E7E7;
+    }
+`;
 
 export default function SelectQuestion() {
     const location = useLocation(); // SelectExamPage.js에서 선택된 시험 받아오기
@@ -14,6 +169,10 @@ export default function SelectQuestion() {
     const [search, setSearch] = useState(""); // 검색어
     const [keywords, setKeywords] = useState([]); // 검색 키워드
     const [selectedQuestionCount, setSelectedQuestionCount] = useState(20);// 선택된 문제 문항 수 
+    const [customQuestionCount, setCustomQuestionCount] = useState(""); // 문항 수 직접 입력을 위한 상태 변수
+    // 미리 정의된 문항 수 버튼을 위한 상태 변수
+const [selectedPredefinedCount, setSelectedPredefinedCount] = useState(selectedQuestionCount);
+const [selected, setSelected] = useState(false);
     const [questions, setQuestions] = useState([]);
     const [showCustomCountInput, setShowCustomCountInput] = useState(false); // "기타" 버튼을 클릭했는지 여부
 
@@ -33,25 +192,6 @@ export default function SelectQuestion() {
     }, [examId]);
     
 
-    // 키워드 추가
-    const handlePushKeyword = (e) => {
-        if ((e.key === 'Enter' || e.type === 'click') && !e.nativeEvent.isComposing) {
-            e.preventDefault();
-            if (search.trim() !== '') { 
-                setKeywords([...keywords, search.trim()]); // 입력된 단어를 키워드 배열에 추가
-                setSearch(''); // 검색어를 초기화
-            }
-        }
-    };
-
-
-  
-    // 키워드 삭제
-    const handelDeleteKeyword = (index) => { 
-        const deleteKeywords = [...keywords];
-        deleteKeywords.splice(index, 1); // 선택된 인덱스의 키워드를 제거
-        setKeywords(deleteKeywords); // 변경된 키워드 배열을 업데이트
-    }
 
     // 태그 선택
     const handleTagClick = (tagGroup, tag) => {
@@ -63,20 +203,45 @@ export default function SelectQuestion() {
         }
     }
 
-    // 문항 수 선택
-    const handleQuestionCountClick = (e) => {
-        const count = e.target.value;
-        if (count === "기타") {
-            setShowCustomCountInput(true);
-        } else {
-            setSelectedQuestionCount(parseInt(count));
+    // 키워드 추가
+    const handlePushKeyword = (e) => {
+        if ((e.key === 'Enter' || e.type === 'click' || e.key === ' ') && !e.nativeEvent.isComposing) {
+            e.preventDefault();
+            if (search.trim() !== '') {
+                setKeywords([...keywords, search.trim()]); // 입력된 단어를 키워드 배열에 추가
+                setSearch(''); // 검색어를 초기화
+            }
         }
+    };
+    
+    
+
+  
+    // 키워드 삭제
+    const handelDeleteKeyword = (index) => { 
+        const deleteKeywords = [...keywords];
+        deleteKeywords.splice(index, 1); // 선택된 인덱스의 키워드를 제거
+        setKeywords(deleteKeywords); // 변경된 키워드 배열을 업데이트
     }
 
-    // 커스텀 문항 수 입력
+
+
     const handleCustomQuestionCountChange = (e) => {
-        setSelectedQuestionCount(parseInt(e.target.value));
+        const value = e.target.value.trim(); // 입력된 값의 양 끝 공백 제거
+        const numberValue = value === "" ? 0 : Number(value); // 문자열을 숫자로 변환
+        setCustomQuestionCount(numberValue); // 상태 업데이트
+        setSelectedQuestionCount(numberValue); // 선택된 문제 수도 업데이트
     }
+    
+    
+    
+    
+
+// 문항 수 선택 핸들러
+const handleQuestionCountClick = (count) => {
+    setSelectedQuestionCount(count);
+    setSelectedPredefinedCount(count);
+}
 
     // 유형 정보 API 전달
     const handleSubmit = async (e) => {
@@ -112,81 +277,99 @@ export default function SelectQuestion() {
         }   
     }
 
-    return(
-        <div className="select-question-container">
-            <div>
-                <h1 style={{ textAlign: 'center' }}>{examTitle}</h1>
-                <div className="tags">
-                {Object.keys(tags).map(tagGroup => (
-                    <div key={tagGroup}>
-                        <span>{tagGroup} </span>
-                        {tags[tagGroup].map(tag => (
-                            <button
-                                key={tag}
-                                type="button"
-                                onClick={() => handleTagClick(tagGroup, tag)} 
-                                className={selectedTags.some(item => item.tagGroup === tagGroup && item.tag === tag) ? "selected-tag" : ""}
-                            >
-                                {tag}
-                            </button>
-                        ))}
-                    </div>
-                ))}
-                </div>
-                <div className="selectWord-container">
-                    <span> 키워드 검색  </span>
-                    <input
+    return (
+        <div>
+            <Title>{examTitle}</Title>
+            <Container>
+                <TagsContainer>
+                    {Object.keys(tags).map(tagGroup => (
+                        <div key={tagGroup}>
+                            <p>{tagGroup}</p>
+                            {tags[tagGroup].map(tag => (
+                                <TagButton
+                                    key={tag}
+                                    selected={selectedTags.some(item => item.tagGroup === tagGroup && item.tag === tag)}
+                                    onClick={() => handleTagClick(tagGroup, tag)}
+                                >
+                                    {tag}
+                                </TagButton>
+                            ))}
+                        </div>
+                    ))}
+                </TagsContainer>
+                <div>
+                    <p> 키워드 검색  </p>
+                    <KeywordInput
                         type="text"
                         value={search}
+                        placeholder="원하는 문제의 태그를 입력해주세요."
                         onChange={(e) => setSearch(e.target.value)}
                         onKeyDown={handlePushKeyword}
                     />
-                   <img
+                    <SearchImg
                         src="/img/검색돋보기.png"
-                        style={{ width: '15px', position: 'relative', marginRight: '20px', border: '1px solid #ccc' }}
-                        onClick={handlePushKeyword} // 이미지 클릭 시 검색 키워드 추가 
+                        alt="search Img"
+                        onClick={handlePushKeyword}
                     />
                     <br />
-                    <div className="keyword-list">
+                    <KeywordList>
                         {keywords.map((keyword, index) => (
-                            <div key={index} className="keyword-container">
-                                <span className="keyword">{keyword}</span>
-                                <button type="button" onClick={()=> handelDeleteKeyword(index)}>x</button>
-                            </div>
+                            <KeywordContainer key={index}>
+                                <Keyword>
+                                   {keyword} 
+                                     <DeleteKeywordImg src="/img/x버튼.png" alt="delete img" onClick={() => handelDeleteKeyword(index)} />
+                                </Keyword>
+                            </KeywordContainer>
                         ))}
-                    </div>
+                    </KeywordList>
                 </div>
                 <br />
-                <div className="question-count">
-                    <span>문항수  </span>
-                    <div className="custom-count-input" style={{ display: 'inline-flex', marginRight: '10px' }}>
-                        <input
+                <QuestionCount>
+                    <p> 문제 갯수 </p>
+                    <div style={{ display: 'inline-flex'}}>
+                        {/* <button>
+                                        <CountInput
                             type="number"
-                            value={selectedQuestionCount}
+                            value={customQuestionCount}
                             placeholder="직접 입력"
                             onChange={handleCustomQuestionCountChange}
-                            min={1} // 최소값 설정
-                            style={{ backgroundColor: '#E8F6F6', color: '#2D2C2B', border: 'none', borderRadius: '5px', padding: '5px 10px', cursor: 'pointer', marginRight: '5px', width: '30px' }}
+                            min={1}
                         />
+                        </button> */}
                     </div>
+                    {/*숫자만 입력 가능하게*/}
+                    <CountInput
+                        type="text" pattern="[0-9]*" 
+                        value={customQuestionCount || ""}
+                        placeholder={customQuestionCount === "" ? "직접 입력" : ""}
+                        onChange={handleCustomQuestionCountChange}
+                        min={1}
+                        onClick={() => setSelectedQuestionCount(parseInt(customQuestionCount))}
+                    />
+
+
                     {[10, 20, 30, 40].map(count => (
-                        <button 
+                            <CountButton
                             key={count}
-                            value={count}
-                            onClick={handleQuestionCountClick}
-                            className={selectedQuestionCount === count ? "selected-count" : ""}
+                            type="button"
+                            selected={selectedPredefinedCount === count}
+                            onClick={() => handleQuestionCountClick(count)}
                         >
-                            {count}
-                        </button>
+                            {count} 문제
+                        </CountButton>
                     ))}
-                    <button onClick={() => setSelectedQuestionCount(0)} className={selectedQuestionCount === 0 ? "selected-count" : ""}> 전체 </button>
-                   
-                </div>
+
+                    {/* 전체 검색 */}
+                    {/* <CountButton onClick={() => setSelectedQuestionCount(0)} selected={selectedQuestionCount === 0}> 전체 </CountButton> */}
+                </QuestionCount>
                 <br />  
-                <input type="submit" onClick={handleSubmit} value="문제 검색" />
-            </div>
-            <hr />
+                <SubmitButtonContainer>
+                    <SubmitButton type="submit" value="문제 검색" onClick={handleSubmit} />
+                </SubmitButtonContainer>
+
+                </Container>
             <ShowQuestionList questions={questions}/>
         </div>
     );
+    
 }

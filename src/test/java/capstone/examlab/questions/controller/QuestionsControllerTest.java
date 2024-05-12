88d0c9fc76f3430cd5329debd61cf82e2762184c
kimjisu <comjisu0311@naver.com>
2024-05-12 15:19:52 +0900

    feat: Questions 조건검색 검증 테스트 코드 추가 #121

--- src/test/java/capstone/examlab/questions/controller/QuestionsControllerTest.java
+++ src/test/java/capstone/examlab/questions/controller/QuestionsControllerTest.java
@@ -2,104 +2,81 @@ package capstone.examlab.questions.controller;
 
 
 import capstone.examlab.exams.repository.ExamRepository;
-import capstone.examlab.questions.repository.QuestionsRepository;
+import capstone.examlab.exams.service.ExamsService;
+import capstone.examlab.questions.dto.QuestionDto;
+import capstone.examlab.questions.dto.QuestionsListDto;
+import capstone.examlab.questions.dto.search.QuestionsSearchDto;
 import capstone.examlab.questions.service.QuestionsService;
+import capstone.examlab.users.argumentresolver.LoginUserArgumentResolver;
+import capstone.examlab.users.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.Collections;
 import java.util.List;
 
-@SpringBootTest
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(QuestionsController.class)
+@Tag("basic_test")
 public class QuestionsControllerTest {
-    /*
+
+    @Autowired
+    private MockMvc mockMvc;
+
     @MockBean
     private QuestionsService questionsService;
 
     @MockBean
-    private ExamRepository examRepository;
+    private ExamsService examsService;
+
+    @Autowired
+    private LoginUserArgumentResolver loginUserArgumentResolver;
 
     @MockBean
-    private QuestionsRepository questionsRepository;
+    private UserRepository userRepository;
 
-    private final Long existExamId = 1L;
+    @MockBean
+    private ExamRepository examRepository;
 
+    private final Long existExamId = 0L;
     private final List<String> existTags = List.of("상황", "법");
 
     @BeforeEach
     public void beforeTest() {
-        when(examRepository.existsById(existExamId)).thenReturn(true);
-        when(questionsService.findByDriverLicenseQuestions(any(), any())).thenReturn(new QuestionsList());
-        when(questionsRepository.existsByTags(existTags.get(0))).thenReturn(true);
-        when(questionsRepository.existsByTags(existTags.get(1))).thenReturn(true);
+        when(examRepository.existsByExamId(existExamId)).thenReturn(true);
     }
 
     @Test
     void validQuestionControllerTest() throws Exception {
+        List<QuestionDto> questions = Collections.emptyList();
+        QuestionsListDto questionsListDto = new QuestionsListDto(questions);
+        when(questionsService.searchFromQuestions(eq(existExamId), any(QuestionsSearchDto.class)))
+                .thenReturn(questionsListDto);
+
+        // 해당 되는 내용 없을시 404 에러
         mockMvc.perform(get("/api/v1/exams/{examId}/questions", existExamId)
-                        .queryParam("tags", existTags.get(0))
-                        .queryParam("tags", existTags.get(1))
+                        .queryParam("tags_category", existTags.get(0))
+                        .queryParam("tags_category", existTags.get(1))
                         .queryParam("count", String.valueOf(3))
-                        .queryParam("includes", "고속도로"))
-                .andExpect(status().isOk());
+                        .queryParam("includes", "장농면허어디감"))
+                .andExpect(status().isNotFound());
+
+        // 정규식 검증에 걸린 경우 400에러
+        mockMvc.perform(get("/api/v1/exams/{examId}/questions", existExamId)
+                        .queryParam("tags_!category", "상@@황")
+                        .queryParam("tags_category", "법_법")
+                        .queryParam("count", String.valueOf(3))
+                        .queryParam("includes", "운전면허증"))
+                .andExpect(status().isBadRequest());
     }
-
-    // 없는 문제 ID로 요청을 보내면 400 Bad Request를 반환하는지 테스트
-    @Test
-    void validExamIdTest() throws Exception {
-        // When
-        Long examId = 0L;
-
-        //Then
-        mockMvc.perform(get("/api/v1/exams/{examId}/questions", examId))
-                .andExpect(status().isBadRequest());
-
-        mockMvc.perform(get("/api/v1/exams/{examId}/questions", existExamId))
-                .andExpect(status().isOk());
-    }
-
-    // 없는 태그로 요청을 보내면 400 Bad Request를 반환하는 테스트
-    @Test
-    void invalidTagsTest() throws Exception {
-        // Then
-        mockMvc.perform(get("/api/v1/exams/{examId}/questions", existExamId)
-                        .queryParam("tags", existTags.get(0))
-                        .queryParam("tags", "없는태그"))
-                .andExpect(status().isBadRequest());
-
-        mockMvc.perform(get("/api/v1/exams/{examId}/questions", existExamId)
-                        .queryParam("tags", existTags.get(0))
-                        .queryParam("tags", existTags.get(1)))
-                .andExpect(status().isOk());
-    }
-
-    // 양수가 아닌 count로 요청을 보내면 400 Bad Request를 반환하는 테스트
-    @Test
-    void invalidCountTest() throws Exception {
-        // Then
-        mockMvc.perform(get("/api/v1/exams/{examId}/questions", existExamId)
-                        .queryParam("count", String.valueOf(-3)))
-                .andExpect(status().isBadRequest());
-
-        mockMvc.perform(get("/api/v1/exams/{examId}/questions", existExamId)
-                        .queryParam("count", String.valueOf(3)))
-                .andExpect(status().isOk());
-    }
-
-    // 글자수 및 빈칸 적용된 유효하지않은 inlucds 요청시 400 Bad Request를 반환하는 테스트
-    @Test
-    void invalidIncludesTest() throws Exception {
-        // Then
-        mockMvc.perform(get("/api/v1/exams/{examId}/questions", existExamId)
-                        .queryParam("includes", "두 단어"))
-                .andExpect(status().isBadRequest());
-
-        mockMvc.perform(get("/api/v1/exams/{examId}/questions", existExamId)
-                        .queryParam("includes", "한"))
-                .andExpect(status().isBadRequest());
-
-        mockMvc.perform(get("/api/v1/exams/{examId}/questions", existExamId)
-                        .queryParam("includes", "한단어한글자이상"))
-                .andExpect(status().isOk());
-    }*/
 }

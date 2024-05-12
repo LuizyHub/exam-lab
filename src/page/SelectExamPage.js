ba77913f22565f7f4fcf650a9b5e7edc374d2484
binnay <125789787+binnay@users.noreply.github.com>
2024-05-12 23:28:46 +0900

    [FE] merge error 해결 (#127)
    * refactor : EditExam페이지 내 사이드바 추가
    
    * refactor : 시험 속성 CRUD 보완
    
     - 속성과 시험 삭제시, 한번 더 삭제 메세지를 띠울 수 있다.
     - 사용자가 이해하기 용이하도록 속성과 태그에 default로 난이도-상,중,하를 준다.
     - 사용자가 이해하기 용이하도록 설명글을 제공한다.
    
    * fix : 옵션들의 번호를 출력할 수 있다.
    
    * refactor : 문제 검색 페이지 수정
    
    * style : 전반적인 페이지 스타일 적용
    
    - styled-components 사용
    
    * refactor: 파일 여부에 따른 버튼 설정
    
    - AI 문제 생성에서 파일이 없으면 업로드 버튼만 뜨고, 파일이 있으면 파일 삭제 버튼과 문제 생성 버튼이 뜬다.
    
    * fix : style-components 에러 해결
    
    * fix : styled-components 오류 수정
    
    * factor : 영어 문제 데이터 처리
    
    - box, content
    - h1, h2, h3
    - 표 처리
    
    * fix : styled-components 오류 수정
    
    * Feature : 시험에 대한 문제 갯수도 제공할 수 있다.

--- src/page/SelectExamPage.js
+++ src/page/SelectExamPage.js
@@ -8,6 +8,7 @@ import { isVisibleState } from '../recoil/atoms';
 
 
 const SelectExam = styled.div`
+
    display: flex;
    flex-direction: column;
    margin-left: ${({ $isSidebarOpen }) => $isSidebarOpen ? '250px' : '60px'};
@@ -98,6 +99,8 @@ export default function SelectExamPage() {
   const navigate = useNavigate();
   const [exams, setExams] = useState([]);
   const [modalStates, setModalStates] = useState({}); // 시험별 모달 상태
+  const [examSize, setExamSize] = useState([]);
+
   const isSidebarOpen = useRecoilValue(isVisibleState);
 
   useEffect(() => {
@@ -160,15 +163,15 @@ export default function SelectExamPage() {
         <ExamCreateButton onClick={() => {navigate('/edit')}}> 
           <CreateImg src="/img/추가하기.png" alt="Create Icon" />
         </ExamCreateButton>
-        <div>
+        <div style={{ display: 'flex', flexDirection: 'column' }}> 
           {exams.map(exam => (
             <div key={exam.exam_id}>
               <ExamButton onClick={() => handleExamTypeClick(exam.exam_id, exam.exam_title)}>
-                {exam.exam_title}
+              <p style={{fontSize: '15px',  marginTop:'0px', fontWeight: 'bold'}}>{exam.exam_title} </p>
+                <p style={{fontSize: '12px', marginBottom:'0px'}}> {exam.size}문제 </p>
                 <DeleteImg src="/img/쓰레기통.png" alt="Delete Icon" onClick={(e) => { e.stopPropagation(); handleOpenModal(exam.exam_id); }} />
-              </ExamButton>
+              </ExamButton>  
 
-              
               {modalStates[exam.exam_id] && 
                 <DeleteConfirmModal>
                   <ModalBody>
@@ -180,7 +183,7 @@ export default function SelectExamPage() {
               }
             </div>
           ))}
-        </div>
+      </div>
       </div>
       <NavigationBar />
     </SelectExam>

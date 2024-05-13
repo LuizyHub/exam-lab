3fa2c5329d2aeb670fa974b749c0006b3ae322d8
4Bee <4bee.code@gmail.com>
2024-05-13 20:39:17 +0900

    refactor : 불필요한 파일과 코드를 삭제하고 React-pdf 설치

--- src/App.js
+++ src/App.js
@@ -8,33 +8,32 @@ import ParsingImageTest from "./test/html_parse_img/parsingImageTest"
 import SelectQuestionPage from "./page/SelectQuestionPage"
 import WorkBooks from "./page/WorkBooks";
 
-import AttributeManager from "./components/AttributeManager";
-import EnglishExam from "./test/english/englishExam";
+import Pdf from "./test/Pdf/Pdf";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <RecoilRoot>
-    <BrowserRouter>
+      <BrowserRouter>
         <div>
           <Routes>
             <Route path="/" element={<MainPage />} />
             <Route path="/exams" element={<SelectExam />} />
             <Route path="/exams/create" element={<SelectExamCreate />} />
             <Route path="/selectQuestion" element={<SelectQuestionPage />} />
-            <Route path="/workbooks/create" element={<LabExam />} />
             <Route path="/edit" element={<EditExam />} />
             {/* <Route path="/edit" element={<EditExamRefactory />} /> */}
             <Route path="/htmlParse" element={<ParsingImageTest />} />
             <Route path="/workbooks" element={<WorkBooks />} />
-            {/* 영어문제 테스트를 위한 페이지 */}
-            <Route path="/english" element={<EnglishExam />} />
-            <Route path="/attribute" element={<AttributeManager />} />
+            <Route path="/workbooks/create" element={<LabExam />} />
+
+            {/* 테스트용 */}
+            <Route path="/pdf" element={<Pdf />} />
           </Routes>
         </div>
-      
-    </BrowserRouter>
+
+      </BrowserRouter>
     </RecoilRoot>
   )
 }

411d6aa6a25bf200021c8001449b67ad1b5a2315
luizy <luizy991212@gmail.com>
2024-05-19 16:16:54 +0900

    feature: AI 응답이 실패할 경우 재요청 #140

--- src/main/java/capstone/examlab/ai/dto/Questions.java
+++ src/main/java/capstone/examlab/ai/dto/Questions.java
@@ -1,10 +1,13 @@
 package capstone.examlab.ai.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class Questions {
+
+    @NotNull
     List<Question> questions;
 }

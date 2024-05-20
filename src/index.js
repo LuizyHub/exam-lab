e0a52c951d199e4d42b7751a5966ae06eef07040
binnay <125789787+binnay@users.noreply.github.com>
2024-05-20 23:51:31 +0900

    fix : import reportWebVitals 주석 처리로 인한 배포시 에러 수정

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 import App from './App';
-import { RecoilRoot } from 'recoil';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <RecoilRoot>
   <App />
-  </RecoilRoot>
   // </React.StrictMode>
 );
 

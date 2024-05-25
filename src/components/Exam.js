071cfa3e501882160a33a0b96c941192c1862ece
4Bee <4bee.code@gmail.com>
2024-05-25 19:40:54 +0900

    feat: 시험지 저장소와 시험지 제작소 페이지에서 나타나는 저장하기 버튼이 각 페이지 마다 다르게 작동하게 구현

--- src/components/Exam.js
+++ src/components/Exam.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 export default function Exam({ isQuestion, parseImages, renderImages, isCommentary, isCommentaryQuestion }) {
   const printComponent = () => {
     const printableArea = document.getElementById('printableArea');
@@ -36,15 +35,15 @@ export default function Exam({ isQuestion, parseImages, renderImages, isCommenta
             </div>
 
             <div style={{ display: isCommentary ? 'block' : 'none', marginBottom: '10px', border: '1px solid #EBEDEF', padding: '20px', width: '1180px', height: 'auto' }}>
-                <div>{index + 1}. </div>
-                <div style={{ padding: '1px' }}>
-                <div style={{ display: 'flex', marginBottom: '10px' }}><p style={{ margin: '0px' }}>답 :
-                  {String.fromCharCode(9311 + parseInt(item.answers))}
+              <div className="comment-container" style={{display: 'flex'}}>
+                <div className="index" style={{marginRight: '5px'}}><b>{index + 1}.</b> </div>
+                <div className="comment" style={{padding: '1px' }}>
+                <div style={{ marginBottom: '10px' }}><p style={{ margin: '0px' }}><b>답</b> : {String.fromCharCode(9311 + parseInt(item.answers))}
                   {/* { parseInt(item.answers) } AI가 답을 생성할 때 0부터 시작하면 안된다. */}
                 </p></div>
-                  <div style={{ display: 'flex' }}><p style={{ margin: '0px' }}>해 : {item.commentary}</p></div>
+                  <div><p style={{ margin: '0px' }}><b>해</b> : {item.commentary}</p></div>
                 </div>
-
+              </div>
             </div>
 
           </div>

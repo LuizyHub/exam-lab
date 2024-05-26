dfe4152d57f0e64707fa8a80c43fab6726046e09
4Bee <4bee.code@gmail.com>
2024-05-26 11:18:04 +0900

    fix : 이미지 로딩 Failed to load resource문제 해결 및 옵션 특수 기호 삽입 시험지 A4크기로 규격 맞춤 세팅

--- src/page/LabExam.js
+++ src/page/LabExam.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Font, PDFDownloadLink, Document, Page, Text, View, StyleSheet, Image } from '@react-pdf/renderer';
-// import "../css/style.css"
-import { handleShuffle } from '../function/shuffleArray'
-import { renderImages, parseImages } from '../function/renderImages'
+import { handleShuffle } from '../function/shuffleArray';
+import { renderImages, parseImages } from '../function/renderImages';
 import { useRecoilValue } from 'recoil';
 import { isVisibleState } from '../recoil/atoms';
 import NavigationBar from '../components/NavigationBar';
@@ -12,18 +11,18 @@ import '../css/labexam.css';
 import axios from "axios";
 import Exam from "../components/Exam";
 
-const LabExamContent =
-  styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-left: ${({ $isSidebarOpen }) => $isSidebarOpen ? '250px' : '60px'};
-    transition: margin-left 0.3s ease;
+const LabExamContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: ${({ $isSidebarOpen }) => $isSidebarOpen ? '250px' : '60px'};
+  transition: margin-left 0.3s ease;
 `;
 
 Font.register({
   family: 'NanumGothic-Regular',
-  src: '/font/NanumGothic-Regular.ttf', // Font.register 폰트 파일의 경로는 기본 public으로 시작
+  src: '/font/NanumGothic-Regular.ttf',
 });
+
 const styles = StyleSheet.create({
   page: {
     flexDirection: 'column',
@@ -36,14 +35,19 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     fontFamily: 'NanumGothic-Regular',
     fontSize: 10,
-    width: '90%',
+    width: '100%',
     height: 'auto',
     marginBottom: 10,
-    border: '1px solid #F9F9FA',
+    // border: '1px solid #F9F9FA',
   },
   image: {
-    width: 80,
-    height: 80,
+    width: '40%',
+    height: 'auto',
+    marginBottom: 5,
+  },
+  fullWidthImage: {
+    width: '40%',
+    height: 'auto',
   },
   input: {
     marginBottom: 10,
@@ -61,73 +65,89 @@ const styles = StyleSheet.create({
   },
 });
 
-const PdfDocument = ({ isQuestion, isCommentary }) => (
-  <Document>
-    <Page size="A4" style={styles.page}>
-      {isQuestion.map((question, index) => (
-        <div key={index} style={styles.section}>
-          <Text>{index + 1}. {question.question}</Text>
-          {question.question_images_out && question.question_images_out.length > 0 && (
-            question.question_images_out.map((image, imageIndex) => (
-              // 이미지 URL의 유효성 검사
-              image.url ? (
-                <Image key={imageIndex} src={image.url} style={styles.image} />
-              ) : null
-            ))
-          )}
-          <Text>{question.options}</Text>
-        </div>
-      ))}
-    </Page>
-  </Document>
-);
+const PdfDocument = ({ isQuestion, isCommentary }) => {
+  const [reloadImage, setReloadImage] = useState({}); // 이미지 재로드 상태 추가
 
+  // 이미지 로드 오류 처리 함수 추가
+  const handleImageError = (index) => {
+    setReloadImage((prevState) => ({
+      ...prevState,
+      [index]: (prevState[index] || 0) + 1
+    }));
+  };
+
+  return (
+    <Document>
+      <Page size="A4" style={styles.page} wrap>
+        {isQuestion.map((question, index) => (
+          <View key={index} style={styles.section}>
+            <Text>{index + 1}. {question.question}</Text>
+            {question.question_images_out && question.question_images_out.length > 0 && (
+              question.question_images_out.map((image, imageIndex) => (
+                image.url ? (
+                  <Image 
+                    key={imageIndex} 
+                    src={`${image.url}?reload=${reloadImage[imageIndex] || 0}`} // 이미지 URL에 쿼리 파라미터 추가
+                    style={styles.image}
+                    onError={() => handleImageError(imageIndex)} // 이미지 로드 오류 처리 핸들러 추가
+                  />
+                ) : null
+              ))
+            )}
+          {question.options.map((option, optionIndex) => (
+            <Text key={optionIndex}>
+              {String.fromCharCode(9312 + optionIndex)} {option}
+            </Text>
+          ))}
+          </View>
+        ))}
+      </Page>
+    </Document>
+  );
+};
 
 export default function LabExam() {
-
   const location = useLocation();
   const { selectedQuestions } = location.state;
   const workbookId = location.state.workbookId;
 
   const [isWorkBook, setWorkBook] = useState([]);
   const [isNewWorkBook, setNewWorkBook] = useState("");
-  const [isQuestion, setIsQuestion] = useState([]); //상태변수 이름 변경 필요
-  const [isCommentaryQuestion, setIsCommentaryQuestion] = useState(true);//상태변수 이름 변경 필요
-  const [isCommentary, setIsCommentary] = useState(false); //상태변수 이름 변경 필요
+  const [isQuestion, setIsQuestion] = useState([]);
+  const [isCommentaryQuestion, setIsCommentaryQuestion] = useState(true);
+  const [isCommentary, setIsCommentary] = useState(false);
   const isSidebarOpen = useRecoilValue(isVisibleState);
 
   useEffect(() => {
     if (workbookId) {
       axios.get(`/api/v1/workbooks/${workbookId}`)
         .then(response => {
-          console.log(response.data);
           const res = response.data.message;
           const content = res.content;
           setWorkBook(res);
-          setIsQuestion(content.questions);//문제
+          setIsQuestion(content.questions);
         })
         .catch(error => {
           console.log(error);
         });
     } else {
-      setIsQuestion(selectedQuestions);//문제 선택 페이지
+      setIsQuestion(selectedQuestions);
     }
-  }, [workbookId])
+  }, [workbookId]);
 
   const handleInputChange = (event) => {
     setNewWorkBook(event.target.value);
   };
 
-  //버튼 클릭 시 답지 생성 토글
   const handleCommentary = () => {
     setIsCommentaryQuestion(false);
     setIsCommentary(true);
-  }
+  };
 
   const handleQuestion = () => {
     setIsCommentaryQuestion(true);
     setIsCommentary(false);
-  }
+  };
 
   const postData = async () => {
     try {
@@ -135,13 +155,12 @@ export default function LabExam() {
         title: isNewWorkBook,
         summary: "not",
         content: {
-          questions: isQuestion, // Use the state variable here
-          size: isQuestion.length // Size of questions array
+          questions: isQuestion,
+          size: isQuestion.length
         }
       };
-      console.log(data);
       const response = await axios.post('/api/v1/workbooks', data);
-      console.log(response.data); // Handle response as needed
+      console.log(response.data);
     } catch (error) {
       console.error('Error posting data:', error);
     }
@@ -154,7 +173,6 @@ export default function LabExam() {
         <div className="lab-exam">
           <div className="button-container">
             <div className="button-main">
-              {/* <Exam /> */}
               <div>
                 <input className="title" placeholder="시험지 이름 입력" value={isWorkBook.title} onChange={handleInputChange} />
                 <div id="button-pdf">
@@ -166,26 +184,20 @@ export default function LabExam() {
               </div>
             </div>
             <div className="button-bottom-container">
-
               <div className="button-selection">
                 <button onClick={handleQuestion} style={{ textDecoration: isCommentaryQuestion ? 'underline' : 'none' }}>문제</button>
                 <button onClick={handleCommentary} style={{ textDecoration: isCommentary ? 'underline' : 'none' }}>해설지</button>
               </div>
-
               <div className="button-bottom-sub">
                 <button id="button-shuffle" onClick={() => handleShuffle(isQuestion, setIsQuestion)}>문제 셔플</button>
                 <button>1쪽</button>
                 <button style={{ marginLeft: '1%' }}>2쪽</button>
               </div>
-              {/* <button onClick={console.log(selectedQuestions)}>test</button> */}
             </div>
           </div>
-
           <Exam isQuestion={isQuestion} parseImages={parseImages} renderImages={renderImages} isCommentary={isCommentary} isCommentaryQuestion={isCommentaryQuestion} />
         </div>
-
-      </LabExamContent >
+      </LabExamContent>
     </>
-
   );
 }
